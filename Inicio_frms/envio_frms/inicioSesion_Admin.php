760c044c880ce49ba031882989dc61d793bd33bf
Pablo <you@example.com>
2023-08-03 13:34:09 -0600

    Implementacion de token en remember v1.7

--- Inicio_frms/envio_frms/inicioSesion_Admin.php
+++ Inicio_frms/envio_frms/inicioSesion_Admin.php
@@ -57,7 +57,7 @@ if (isset($_POST['emailAL']) && isset($_POST['passAL'])) {
 
     if ($httpStatusCode == 200) {
         if ($data !== null) {
-
+            echo $response;
             echo $Remember;
             if($Remember == true)
             {
