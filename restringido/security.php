a590859c11f7caa916f9979378420cdf197f457c
Pablo <you@example.com>
2023-08-03 20:53:58 -0600

    Implementacion de token en remember v2.12

--- restringido/security.php
+++ restringido/security.php
@@ -4,20 +4,4 @@ if(!isset($_COOKIE['SessionToken']))
 {
 	header("location: ../index.php");
 }
-
-$token = $_COOKIE['SessionToken'];
-list($header, $payload, $signature) = explode('.', $token);
-$payload = base64_decode(str_pad(strtr($payload, '-_', '+/'), strlen($payload) % 4, '=', STR_PAD_RIGHT));
-$payload = json_decode($payload);
-
-$_SESSION['email'] = $payload->Email;
-$_SESSION['nombre'] = $payload->name;
-$_SESSION['id'] = $payload->sub;
-
-echo $_SESSION['email'];
-if($_SESSION['nombre'] == null)
-{
-    $_SESSION['nombre'] = "No hay nombre";
-}
-echo $_SESSION['id'];
 ?>
