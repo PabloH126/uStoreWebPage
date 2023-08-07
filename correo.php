2047bbfa4b0e5562ee9ee4368249a47da81f207a
PabloH126 <pabo120603@outlook.com>
2023-08-07 11:41:57 -0600

    prueba de token v1.17

--- correo.php
+++ correo.php
@@ -2,20 +2,20 @@
 require 'vendor/autoload.php';
 
 session_start();
-if(isset($_SESSION['claveE'])) {
+if (isset($_SESSION['claveE'])) {
     // Información del correo electrónico del administrador
     $email_administrador = $_SESSION['emailA'];
-    $templateId = "d-4d1b3a15ba034f30a0132dfa9f983906";        
+    $templateId = "d-4d1b3a15ba034f30a0132dfa9f983906";
 
     $datos = array(
         'name' => $_SESSION['nombreA'],
         'clave' => $_SESSION['claveE']
     );
 
-    $email = new \SendGrid\Mail\Mail(); 
+    $email = new \SendGrid\Mail\Mail();
     $email->setFrom("ustoreceo@gmail.com", "uStore");
     $email->setSubject("Confirmación de cuenta - Código de verificación");
-    $email->addTo($email_administrador, $_SESSION['nombreA']);        
+    $email->addTo($email_administrador, $_SESSION['nombreA']);
     $email->setTemplateId($templateId);
     $email->addDynamicTemplateDatas($datos);
 
@@ -26,48 +26,47 @@ if(isset($_SESSION['claveE'])) {
         $response->statusCode() . "\n";
         $response->headers();
         $response->body() . "\n";
-        
+
     } catch (Exception $e) {
-        echo 'Caught exception: '. $e->getMessage() ."\n";
+        echo 'Caught exception: ' . $e->getMessage() . "\n";
     }
 
     header('Location: Inicio_frms/claveAdmins.php');
-} 
-else{
+} else {
     //Asignación de variables de datos
     $_SESSION['nombreA'] = $_POST['nombreA'];
-	$_SESSION['apellidoA'] = $_POST['apellidoA'];
-	$_SESSION['emailA'] = $_POST['emailA'];
+    $_SESSION['apellidoA'] = $_POST['apellidoA'];
+    $_SESSION['emailA'] = $_POST['emailA'];
 
-	//confirmar que las contraseñas son iguales
-	if($_POST['passA'] == $_POST['repassA']){
+    //confirmar que las contraseñas son iguales
+    if ($_POST['passA'] == $_POST['repassA']) {
 
-		$contrasena = $_POST['passA'];
-		$contra = md5(md5($contrasena));
+        $contrasena = $_POST['passA'];
+        $contra = md5(md5($contrasena));
         $_SESSION['passA'] = $contra;
 
         //Datos del remitente
         $header = "From: uStore <uStore@gmail.com>";
 
         // Generar una clave aleatoria
-        $clave = substr(md5(mt_rand()), 0,8);
+        $clave = substr(md5(mt_rand()), 0, 8);
         $clave_mayus = strtoupper($clave);
         $_SESSION['claveE'] = $clave_mayus;
 
         // Información del correo electrónico del administrador
         $email_administrador = $_POST['emailA'];
         $asunto = "Clave de registro";
-        $templateId = "d-4d1b3a15ba034f30a0132dfa9f983906";        
+        $templateId = "d-4d1b3a15ba034f30a0132dfa9f983906";
 
         $datos = array(
             'name' => $_POST['nombreA'],
             'clave' => $_SESSION['claveE']
         );
 
-        $email = new \SendGrid\Mail\Mail(); 
+        $email = new \SendGrid\Mail\Mail();
         $email->setFrom("ustoreceo@gmail.com", "uStore");
         $email->setSubject("Confirmación de cuenta - Código de verificación");
-        $email->addTo($email_administrador, $_POST['nombreA']);        
+        $email->addTo($email_administrador, $_POST['nombreA']);
         $email->setTemplateId($templateId);
         $email->addDynamicTemplateDatas($datos);
 
@@ -78,14 +77,14 @@ else{
             $response->statusCode() . "\n";
             $response->headers();
             $response->body() . "\n";
-            
+
         } catch (Exception $e) {
-            echo 'Caught exception: '. $e->getMessage() ."\n";
+            echo 'Caught exception: ' . $e->getMessage() . "\n";
         }
 
         header('Location: Inicio_frms/claveAdmins.php');
-	} else {
-		echo "Error. las contraseñas no coninciden.";
+    } else {
+        echo "Error. las contraseñas no coninciden.";
     }
 }
 
--- correoContrasena.php
+++ correoContrasena.php
@@ -3,63 +3,63 @@
 require 'vendor/autoload.php';
 
 session_start();
-    $emailRecuperacion = $_POST['emailRec'];
+$emailRecuperacion = $_POST['emailRec'];
 
-    $ch = curl_init();
+$ch = curl_init();
 
-    curl_setopt($ch, CURLOPT_URL, "https://ustoreapi.azurewebsites.net/api/Register/Recover");
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, "https://ustoreapi.azurewebsites.net/api/Register/Recover");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-    $data = [
-        'email' => $emailRecuperacion
-    ];
+$data = [
+    'email' => $emailRecuperacion
+];
 
-    $jsonData = json_encode($data);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+$jsonData = json_encode($data);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
 
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        'Content-Type: application/json',
-        'Content-Length: ' . strlen($jsonData),
-    ]);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($jsonData),
+]);
 
-    $response = curl_exec($ch);
+$response = curl_exec($ch);
 
-    if ($response === false) {
-        echo 'Error: ' . curl_error($ch);
-    } else {
-        $httpStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+if ($response === false) {
+    echo 'Error: ' . curl_error($ch);
+} else {
+    $httpStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+}
+
+$data = json_decode($response, true);
+
+curl_close($ch);
+
+$recuperacionLink = "https://ustoree.azurewebsites.net/Inicio_frms/envio_frms/actualizarContraA.php?token=" . $data['token'];
+
+$templateId = "d-018214066b7d401f965a271dd1dd520b";
+
+if ($httpStatusCode != 404) {
+    $email = new \SendGrid\Mail\Mail();
+    $email->setFrom("ustoreceo@gmail.com", "uStore");
+    $email->setSubject("Recuperacion de cuenta");
+    $email->addTo($emailRecuperacion);
+    $email->setTemplateId($templateId);
+    /*$email->addDynamicTemplateDatas($recuperacionLink);*/
+
+    $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+
+    try {
+        $response = $sendgrid->send($email);
+        $response->statusCode() . "\n";
+        $response->headers();
+        $response->body() . "\n";
+
+    } catch (Exception $e) {
+        echo 'Caught exception: ' . $e->getMessage() . "\n";
     }
-    
-    $data = json_decode($response, true);
+}
 
-    curl_close($ch);
-
-    $recuperacionLink = "https://ustoree.azurewebsites.net/Inicio_frms/envio_frms/actualizarContraA.php?token=" . $data['token'];
-    
-    $templateId = "d-018214066b7d401f965a271dd1dd520b";
-    
-    if($httpStatusCode != 404)
-    {
-        $email = new \SendGrid\Mail\Mail();
-        $email->setFrom("ustoreceo@gmail.com", "uStore");
-        $email->setSubject("Recuperacion de cuenta");
-        $email->addTo($emailRecuperacion);
-        $email->setTemplateId($templateId);
-        $email->addDynamicTemplateDatas($recuperacionLink);
-
-        $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
-    
-        try {
-            $response = $sendgrid->send($email);
-            $response->statusCode() . "\n";
-            $response->headers();
-            $response->body() . "\n";
-            
-        } catch (Exception $e) {
-            echo 'Caught exception: '. $e->getMessage() ."\n";
-        }
-    }
-    
-    header('Location: Inicio_frms/olvidarContra.php');
+header('Location: correoContrasena.php')
+/*header('Location: Inicio_frms/olvidarContra.php');*/
 ?>
