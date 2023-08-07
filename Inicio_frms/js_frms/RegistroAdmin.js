5a72f906a9b7f0075649ec4f5648078f7d7fef0d
PabloH126 <pabo120603@outlook.com>
2023-08-07 16:31:46 -0600

    Prueba password 8 caracteres

--- Inicio_frms/js_frms/RegistroAdmin.js
+++ Inicio_frms/js_frms/RegistroAdmin.js
@@ -4,7 +4,7 @@ const inputs = document.querySelectorAll('#RegistroAdmin input');
 const expresiones = {
 	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	password: /^.{8,50}$/, // 8 a 50 digitos.
+	password: /^.{8,}$/, // 8 a 50 digitos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 }
 
@@ -63,7 +63,15 @@ const validarRePassword = () => {
 	const inputPassword = document.getElementById('passA');
 	const inputRePassword = document.getElementById('repassA');
 
-	if(inputPassword.value !== inputRePassword.value)
+	if (inputPassword.value.length < 8) {
+        document.getElementById(`grupo__password`).classList.add('formulario__grupo-incorrecto');
+        document.getElementById(`grupo__password`).classList.remove('formulario__grupo-correcto');
+        document.querySelector(`#grupo__password i`).classList.add('fa-circle-xmark');
+        document.querySelector(`#grupo__password .formulario__input-error`).textContent = "La contraseña debe contener al menos 8 caracteres";
+        document.querySelector(`#grupo__password .formulario__input-error`).classList.add('formulario__input-error-activo');
+        campos['password'] = false;
+	}
+	else if(inputPassword.value !== inputRePassword.value)
 	{
 		document.getElementById(`grupo__repassword`).classList.add('formulario__grupo-incorrecto');
 		document.getElementById(`grupo__repassword`).classList.remove('formulario__grupo-correcto');
