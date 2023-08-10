b4515c0cbf968dd958c40fd1fd20b60ccb807131
PabloH126 <pabo120603@outlook.com>
2023-08-09 22:08:31 -0600

    prueba imagenes nav

--- restringido/centro_comercial/templates/template.menu.php
+++ restringido/centro_comercial/templates/template.menu.php
@@ -6,22 +6,22 @@
 		
 		<div class="final">
 			<div class="bottn" id="tienda">
-				<a href="lista_tiendas.php"><img src="../img/tienda_select.png"></a>
+				<a href="lista_tiendas.php"><img src="https://ustoredata.blob.core.windows.net/webpage/nav/tienda_select.png"></a>
 			</div>
 			<div class="bottn" id="solicitudes">
-				<a href=""><img src="../../img/solicitudes_select.png"></a>
+				<a href=""><img src="https://ustoredata.blob.core.windows.net/webpage/nav/solicitudes_select.png"></a>
 			</div>
 			<div class="bottn" id="ofertas">
-				<a href=""><img src="../../img/ofertas_select.png"></a>
+				<a href=""><img src="https://ustoredata.blob.core.windows.net/webpage/nav/ofertas_select.png"></a>
 			</div>
 			<div class="bottn" id="tendencias">
-				<a href=""><img src="../../img/tendencias_select.png"></a>
+				<a href=""><img src="https://ustoredata.blob.core.windows.net/webpage/nav/tendencias_select.png"></a>
 			</div>
 			<div class="bottn" id="chat">
-				<a href=""><img src="../../img/chat_select.png"></a>
+				<a href=""><https://ustoredata.blob.core.windows.net/webpage/nav/chat_select.png"></a>
 			</div>
 			<div id="ultimo">
-				<a href="../../../../Inicio_frms/index.php"><img src="../../img/account_select.png"></a>	
+				<a href="../../../../Inicio_frms/index.php"><img src="https://ustoredata.blob.core.windows.net/webpage/nav/account_select.png"></a>	
 			</div>
 			
 		</div>
