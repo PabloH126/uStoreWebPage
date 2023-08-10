f3e07119358d6bc879eb44c4b139a0369973be5e
PabloH126 <pabo120603@outlook.com>
2023-08-09 21:22:52 -0600

    Muestreo de tiendas por mall y admin

--- restringido/centro_comercial/andares/tiendas/lista_tiendas.php
+++ restringido/centro_comercial/andares/tiendas/lista_tiendas.php
@@ -1,3 +1,32 @@
+<?php 
+	session_start();
+	require '../../security.php';
+
+	$ch = curl_init();
+
+	curl_setopt($ch, CURLOPT_URL, "https://ustoreapi.azurewebsites.net/api/Tiendas/GetTiendas");
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		'Authorization: Bearer ' . $_COOKIE['SessionToken']
+	));
+	
+	$response = curl_exec($ch);
+	
+	if ($response === false) {
+		echo 'Error: ' . curl_error($ch);
+	} else {
+		$httpStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	}
+	
+	if ($httpStatusCode != 200) {
+		$tiendasError = "Error al intentar recuperar las tiendas. Codigo de respuesta: " . $httpStatusCode;
+	}
+	$tiendas = json_decode($response, true);
+	
+	curl_close($ch);
+	
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,11 +41,24 @@
 
 	<div class="content">
 		<div class="lista">
+			<?php 
+				  if($mallsError != null)
+				  { ?>
+					<p><?php echo $tiendasError;?></p>
+			<?php 
+				  }
+				  else
+				  { 
+					foreach ($tiendas as $tienda)
+					{ ?>
 			<div class="item">
-				<a href=""><img width="60%" class="logo" src="img/BurgerKing.png"></a>
-				<strong class="nombre">Burger King</strong>
+				<a href=""><img width="60%" class="logo" src="<?php echo $tienda['logoTienda']; ?>"></a>
+				<strong class="nombre"><?php echo $tienda['nombreTienda'];?></strong>
 			</div>
-			
+			<?php
+					}
+				  }
+			?>
 
 			<div class="item" id="agregar">
 				<a href="creacion_tiendas.php"><span class="material-symbols-outlined">add</span></a>
