8f5df418d51a18be7d574995dd58926938bd9eb4
PabloH126 <pabo120603@outlook.com>
2023-08-10 12:10:43 -0600

    cambio direccion de nav tiendas

--- restringido/centro_comercial/templates/template.menu.php
+++ restringido/centro_comercial/templates/template.menu.php
@@ -6,7 +6,7 @@
 		
 		<div class="final">
 			<div class="bottn" id="tienda" title="tiendas">
-				<a href="lista_tiendas.php"><img src="https://ustoredata.blob.core.windows.net/webpage/nav/tienda_select.png"></a>
+				<a href="centro_comercial/andares/tiendas/lista_tiendas.php?id=<?php echo $mall['idCentroComercial']?>"><img src="https://ustoredata.blob.core.windows.net/webpage/nav/tienda_select.png"></a>
 			</div>
 			<div class="bottn" id="solicitudes">
 				<a href=""><img src="https://ustoredata.blob.core.windows.net/webpage/nav/solicitudes_select.png"></a>
