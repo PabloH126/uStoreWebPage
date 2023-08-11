922a705de9d770e81d620f486d12b6197686ef3d
PabloH126 <pabo120603@outlook.com>
2023-08-10 20:26:10 -0600

    rehacer creacion tienda 1.2

--- restringido/centro_comercial/andares/tiendas/envio_tienda.php
+++ restringido/centro_comercial/andares/tiendas/envio_tienda.php
@@ -1,3 +1,8 @@
 <?php
-    
+    session_start();
+
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, "https://ustoreapi.azurewebsites.net/api/Tiendas/CreateTienda");
+    curl_setopt($ch, CURLOPT_POST)
 ?>
