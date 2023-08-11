0d1c3ec3e01d4db59e205d3d9e109250969e98c3
PabloH126 <pabo120603@outlook.com>
2023-08-11 12:48:40 -0600

    rehacer creacion tienda ip 1.7.5

--- restringido/centro_comercial/andares/tiendas/envio_tienda.php
+++ restringido/centro_comercial/andares/tiendas/envio_tienda.php
@@ -2,6 +2,8 @@
     session_start();
 
     $logoTienda = $_FILES['logoTienda'];
+
+    //CREATE TIENDA
     $ch = curl_init();
     
     $data = array(
@@ -30,6 +32,8 @@
 
     curl_close($ch);
 
+
+    //CREATE HORARIO
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, "https://ustoreapi.azurewebsites.net/api/Horarios/CreateHorario");
     curl_setopt($ch, CURLOPT_POST, 1);
@@ -46,37 +50,17 @@
         $httpStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     }
 
-    if($httpStatusCode != 201)
+    if($httpStatusCode != 200)
     {
         echo $httpStatusCode;
     }
-    
-    curl_close($ch);
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "");
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        'Authorization: Bearer ' . $_COOKIE['SessionToken']
-    ));
-    
-    $response = curl_exec($ch);
-    
-    if ($response === false) {
-        echo 'Error: ' . curl_error($ch);
-    } else {
-        $httpStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    }
-    
-    curl_close($ch);
 
     function generateArrayHorario($dia, $dataTienda)
     {
         if(isset($_POST['horas' . $dia . 'apertura']) && isset($_POST['minutos' . $dia . 'apertura']) && isset($_POST['am/pm' . $dia . 'apertura'])
          && isset($_POST['horas' . $dia . 'cierre']) && isset($_POST['minutos' . $dia . 'cierre']) && isset($_POST['am/pm' . $dia . 'cierre']))
         {
-            array(
+             return array(
                 "dia" => $dia,
                 "horarioApertura" => $_POST['horas' . $dia . 'apertura'] . ':' . $_POST['minutos' . $dia . 'apertura'] . ' ' . $_POST['am/pm' . $dia . 'apertura'],
                 "horarioCierre" =>  $_POST['horas' . $dia . 'cierre'] . ':' . $_POST['minutos' . $dia . 'cierre'] . ' ' . $_POST['am/pm' . $dia . 'cierre'],
