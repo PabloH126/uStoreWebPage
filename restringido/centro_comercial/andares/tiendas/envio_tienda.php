350efe5edeed41d8275fa132f5dde669ef4c1c15
PabloH126 <pabo120603@outlook.com>
2023-08-10 20:50:27 -0600

    rehacer creacion tienda 1.5

--- restringido/centro_comercial/andares/tiendas/envio_tienda.php
+++ restringido/centro_comercial/andares/tiendas/envio_tienda.php
@@ -2,7 +2,27 @@
     session_start();
 
     $ch = curl_init();
-
-    curl_setopt($ch, CURLOPT_URL, "https://ustoreapi.azurewebsites.net/api/Tiendas/CreateTienda");
-    curl_setopt($ch, CURLOPT_POST)
+    
+    $data = array(
+        'myFile' => new CURLFile('/path/to/file.txt', 'text/plain')
+    );
+    
+    curl_setopt($ch, CURLOPT_URL, "");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Authorization: Bearer ' . _COOKIE['SessionToken']
+    ));
+    
+    response = curl_exec($ch);
+    
+    if (response === false) {
+        echo 'Error: ' . curl_error($ch);
+    } else {
+        httpStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    }
+    
+    curl_close($ch);
 ?>
+
