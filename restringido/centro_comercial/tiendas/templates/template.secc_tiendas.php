64ed69773ce7a993204bbeb759faa1a09983a466
PabloH126 <pabo120603@outlook.com>
2023-08-12 09:43:44 -0600

    acomodo de links? v1.6

--- restringido/centro_comercial/tiendas/templates/template.secc_tiendas.php
+++ restringido/centro_comercial/tiendas/templates/template.secc_tiendas.php
@@ -1,1 +1,1 @@
-<link rel="stylesheet" type="text/css" href="tiendas/css/tiendas.css">
+<link rel="stylesheet" type="text/css" href="https://ustoree.azurewebsites.net/restringido/centro_comercial/tiendas/css/tiendas.css">
