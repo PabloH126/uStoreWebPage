c01762e0cfa992f563869426f9d1ec6a5d6d08a9
PabloH126 <pabo120603@outlook.com>
2023-08-12 17:25:46 -0600

    creacion de perfil tienda 1.1.1

--- restringido/centro_comercial/tiendas/perfil_tienda.php
+++ restringido/centro_comercial/tiendas/perfil_tienda.php
@@ -3,8 +3,8 @@
 <head>
 <meta charset="utf-8">
 	<title>Perfil tienda</title>
-	<?php require("templates/template.styles.php")?>
-	<?php require("tiendas/templates/template.secc_tiendas.php")?>
+	<?php require("../templates/template.styles.php")?>
+	<?php require("templates/template.secc_tiendas.php")?>
 </head>
 <body>
     <?php require("../templates/template.menu.php")?>
