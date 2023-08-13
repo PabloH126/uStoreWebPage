872afca4d9a42a3046d5bb24c9c49b6b36c03aaf
PabloH126 <pabo120603@outlook.com>
2023-08-12 21:36:10 -0600

    disno perfil tienda 1.3.1

--- restringido/centro_comercial/tiendas/perfil_tienda.php
+++ restringido/centro_comercial/tiendas/perfil_tienda.php
@@ -10,27 +10,29 @@
 <body>
     <?php require("../templates/template.menu.php")?>
     <div class="content">
-    <div class="izquierda">
-			<div class="topI">
-				<div class="icon"></div>
-				<div class="nameCat"></div>
-			</div>
-			<div class="botI">
-				<div class="tit"></div>
-				<div class="ban"></div>
-			</div>
-		</div>
-		<div class="derecha">
-			<div class="topD">
-				<div class="info"></div>
-				<div class="info"></div>
-				<div class="info"></div>
-			</div>
-			<div class="botD">
-				<div class="tit"></div>
-				<div class="comnts"></div>
-			</div>
-		</div>
+        <div>
+            <div class="izquierda">
+                <div class="topI">
+                    <div class="icon"></div>
+                    <div class="nameCat"></div>
+                </div>
+                <div class="botI">
+                    <div class="tit"></div>
+                    <div class="ban"></div>
+                </div>
+            </div>
+            <div class="derecha">
+                <div class="topD">
+                    <div class="info"></div>
+                    <div class="info"></div>
+                    <div class="info"></div>
+                </div>
+                <div class="botD">
+                    <div class="tit"></div>
+                    <div class="comnts"></div>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
