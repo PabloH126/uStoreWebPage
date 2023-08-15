9a7b95ea3f79348e2416a29f3e04f193f785da23
PabloH126 <pabo120603@outlook.com>
2023-08-14 18:18:51 -0600

    disno perfil tienda 1.7.2

--- restringido/centro_comercial/tiendas/perfil_tienda.php
+++ restringido/centro_comercial/tiendas/perfil_tienda.php
@@ -49,8 +49,12 @@ session_start();
                     <div class="tit">
                         <h2>Promociones</h2>
                     </div>
-                    <div class="slider">
-
+                    <div class="slider-container">
+                        <div class="slider">
+                            <img src="https://static.wikia.nocookie.net/naruto/images/a/a2/Naruto_Uzumaki_Parte_II_Anime.png/revision/latest?cb=20161013194453&path-prefix=es" alt="">
+                            <img src="https://www.mundodeportivo.com/alfabeta/hero/2021/03/naruto.1677590248.2134.jpg?width=1200" alt="">
+                            <img src="https://www.mundodeportivo.com/alfabeta/hero/2023/03/image-2023-03-23t222927.291.jpg?width=1200&aspect_ratio=16:9" alt="">
+                        </div>
                     </div>
                 </div>
             </div>
