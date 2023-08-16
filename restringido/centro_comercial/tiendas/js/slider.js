a5ff1856c40e53b97a96a453b014852f418a21c6
PabloH126 <pabo120603@outlook.com>
2023-08-16 00:08:16 -0600

    el maldito slider v3

--- restringido/centro_comercial/tiendas/js/slider.js
+++ restringido/centro_comercial/tiendas/js/slider.js
@@ -18,4 +18,45 @@ const btnLeft = document.querySelector(".btn-left"),
       slider = document.querySelector("#slider"),
       sliderImg = document.querySelectorAll(".slider-img");
 
-console.log(sliderImg);
+btnLeft.addEventListener("click", e => moveToLeft())
+btnRight.addEventListener("click", e => moveToRight())
+
+setInterval(() => {
+    moveToRight()
+}, 3000);
+
+
+let operacion = 0,
+    counter = 0,
+    widthImg = 100 / sliderImg.length;
+
+function moveToRight() {
+    if (counter >= sliderImg.length-1) {
+        counter = 0;
+        operacion = 0;
+        slider.style.transform = `translate(-${operacion}%)`;
+        slider.style.transition = "none";
+        return;
+    } 
+    counter++;
+    operacion = operacion + widthImg;
+    slider.style.transform = `translate(-${operacion}%)`;
+    slider.style.transition = "all ease .6s"
+    
+}  
+
+function moveToLeft() {
+    counter--;
+    if (counter < 0 ) {
+        counter = sliderImg.length-1;
+        operacion = widthImg * (sliderImg.length-1)
+        slider.style.transform = `translate(-${operacion}%)`;
+        slider.style.transition = "none";
+        return;
+    } 
+    operacion = operacion - widthImg;
+    slider.style.transform = `translate(-${operacion}%)`;
+    slider.style.transition = "all ease .6s"
+    
+    
+}   
