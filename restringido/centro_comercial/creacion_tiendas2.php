3a779e44ccc0693e0e7abc75eac8d701443c61a3
PabloH126 <pabo120603@outlook.com>
2023-08-18 20:48:54 -0600

    crear tienda 2 v1

--- restringido/centro_comercial/creacion_tiendas2.php
+++ restringido/centro_comercial/creacion_tiendas2.php
@@ -11,5 +11,23 @@
 <?php require("templates/template.menu.php")?>
 	<div class="content">
 		<h1>Creación de tienda</h1>
+        <div>
+            <form>
+                <fieldset>
+                    <legend>Información de Contacto</legend>
+                    
+                    <label for="nombre">Nombre:</label>
+                    <input type="text" id="nombre" name="nombre" required>
+                    
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" required>
+                    
+                    <label for="mensaje">Mensaje:</label>
+                    <textarea id="mensaje" name="mensaje" rows="4" required></textarea>
+                </fieldset>
+                
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
     </div>
 </body>
