20a306951dcfaf0d9d52508cf4f44f323de6ddf9
PabloH126 <pabo120603@outlook.com>
2023-08-21 17:57:35 -0600

    crear tienda 2 c: v3.2.21

--- restringido/centro_comercial/creacion_tiendas2.php
+++ restringido/centro_comercial/creacion_tiendas2.php
@@ -56,28 +56,29 @@
                         <label><strong>Categorías de la tienda</strong></label>
                         <div class="optionsC">
                             
+                            <input type="checkbox" id="alimentosBebidas">
                             <div class="contentC">
-                                <input type="checkbox" id="alimentosBebidas">
+                                
                                 <label for="alimentosBebidas">alimentos y bebidas</label>
                             </div>
 
+                            <input type="checkbox" id="departamentales">
                             <div class="contentC">
-                                <input type="checkbox" id="departamentales">
                                 <label for="departamentales">departamentales</label>
                             </div>
                             
-                            <div class="contentC">
-                                <input type="checkbox" id="bebes">
+                            <input type="checkbox" id="bebes">
+                            <div class="contentC">                                
                                 <label for="bebes">bebes</label>
                             </div>
                             
+                            <input type="checkbox" id="videojuegos">
                             <div class="contentC">
-                                <input type="checkbox" id="videojuegos">
                                 <label for="videojuegos">videojuegos</label>
                             </div>
 
+                            <input type="checkbox" id="vida">
                             <div class="contentC">
-                                <input type="checkbox" id="vida">
                                 <label for="vida">estilo de vida</label>
                             </div>
                         </div>
