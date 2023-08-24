7ba48c8bcb41255b6d20ef6668c44b58c4cc0777
PabloH126 <pabo120603@outlook.com>
2023-08-23 19:53:33 -0600

    intento del slider en formularios 1.6.5 ya por favor

--- restringido/centro_comercial/creacion_tiendas2.php
+++ restringido/centro_comercial/creacion_tiendas2.php
@@ -90,7 +90,7 @@ function CategoriasSelect($categorias)
 
                 <!-- Logo de tienda-->
                 <div class="item" id="item-2">
-                    <!--   <p>2/6</p> -->
+                    <p>2/6</p>
                     <div class="logoT">
                         <label><strong>Logo de la tienda</strong></label>
                         <div class="contentL">
@@ -113,7 +113,7 @@ function CategoriasSelect($categorias)
 
                 <!-- Categorias de tienda-->
                 <div class="item" id="item-3">
-                    <!-- <p>3/6</p> -->
+                    <p>3/6</p>
                     <div class="categorias">
                         <label><strong>Categorías de la tienda</strong></label>
                         <div class="optionsC">
