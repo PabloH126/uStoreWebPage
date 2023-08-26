a5d557058f03db1daf12a77a4186a2693229a678
PabloH126 <pabo120603@outlook.com>
2023-08-25 18:37:21 -0600

    prueba datos_tienda v1.2

--- restringido/centro_comercial/tiendas/edicion_tiendas.php
+++ restringido/centro_comercial/tiendas/edicion_tiendas.php
@@ -279,9 +279,9 @@ function CategoriasSelect($categorias)
             </form>
         </div>
     </div>
-    <script src="js/slider_formularios.js"></script>
-    <script src="js/mostrarImg.js"></script>
-    <script src="js/creacion_tiendas.js"></script>
+    <script src="../js/slider_formularios.js"></script>
+    <script src="../js/mostrarImg.js"></script>
+    <script src="../js/creacion_tiendas.js"></script>
 </body>
 
 </html>
