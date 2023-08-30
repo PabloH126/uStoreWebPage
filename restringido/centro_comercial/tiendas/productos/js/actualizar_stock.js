6b938f5839f833f9e945bea3f4eb95ad65589b15
PabloH126 <pabo120603@outlook.com>
2023-08-30 15:28:30 -0600

    el push de jorge 4

--- restringido/centro_comercial/tiendas/productos/js/actualizar_stock.js
+++ restringido/centro_comercial/tiendas/productos/js/actualizar_stock.js
@@ -1,22 +1,29 @@
+$(document).ready(function() {
+    $(".switch-input").change(function() {
+        var isChecked = $(this).prop("checked");
+        var stockStatus = isChecked ? "Hay Stock" : "Sin Stock";
+        $(this).closest(".stock-switch").find(".stock-status").text(stockStatus);
+    });
+});
+
 document.addEventListener("DOMContentLoaded", function() {
     const switchCheckbox = document.querySelectorAll(".switch-input");
 
     switchCheckbox.forEach(checkbox => {
         checkbox.addEventListener("change", function() {
-            if (this.checked) {
-                updateStock()
-            }
-        })
+            this.value = this.checked ? "1" : "0";
+            updateStock(this.dataset.productoId, this.value);
+        });
     })
 
 });
 
-function updateStock(idProducto, stock)
+async function updateStock(idProducto, stock)
 {
     const formData = new URLSearchParams();
     formData.append('idProducto', idProducto);
     formData.append('stock', stock);
-    fetch('actualizar_stock.php', {
+    const response = await fetch('actualizar_stock.php', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
@@ -25,9 +32,22 @@ function updateStock(idProducto, stock)
     })
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        if(data.status === 'success')
+        {
+            showNotification("")
+        }
     })
     .catch(error => {
         console.error('Error:', error);
     });
+}
+
+function showNotification(message, url) {
+    const notification = document.createElement("div");
+    notification.classList.add("notification");
+    notification.textContent = message;
+    document.body.appendChild(notification);
+    setTimeout(() => {
+        notification.remove();
+    }, 2500);
 }
