63472cd2e91db15a1546cefc4dd78c1f5742e7bc
PabloH126 <pabo120603@outlook.com>
2023-08-30 14:13:08 -0600

    switch stock productos 1.0

--- restringido/centro_comercial/tiendas/productos/lista_productos.php
+++ restringido/centro_comercial/tiendas/productos/lista_productos.php
@@ -56,12 +56,12 @@
 						<strong class="nombre"><?php echo $producto['nombreProducto'];?></strong>
 						<div class="switch">
 							<label class="switch-label">
-					        	<input type="checkbox" class="switch-input" data-producto-id="<?php echo $producto['idProductos']; ?>" <?php echo $producto['stock'] > 0 ? 'checked' : ''; ?>>
-					    	    <span class="slider round"></span>
-						    </label>
-						    <span class="stock-status <?php echo $producto['stock'] > 0 ? 'stock-available' : 'stock-unavailable'; ?>">
-					    	    <?php echo $producto['stock'] > 0 ? 'Hay Stock' : 'Sin Stock'; ?>
-						    </span>
+    	                        <input type="checkbox" class="switch-input" data-producto-id="<?php echo $producto['idProductos']; ?>" <?php echo $producto['stock'] > 0 ? 'checked' : ''; ?>>
+        	                    <span class="slider round"></span>
+            	            </label>
+                	        <span class="stock-status <?php echo $producto['stock'] > 0 ? 'stock-available' : 'stock-unavailable'; ?>">
+                    	        <?php echo $producto['stock'] > 0 ? 'Hay Stock' : 'Sin Stock'; ?>
+                        	</span>
                     	</div>
 					</div>
 			<?php
