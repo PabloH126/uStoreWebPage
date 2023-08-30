6666978ecf297443a9a6dd08bcb7b0ff7f78abf5
PabloH126 <pabo120603@outlook.com>
2023-08-30 14:41:50 -0600

    el push de jorge 3

--- restringido/centro_comercial/tiendas/productos/js/actualizar_stock.js
+++ restringido/centro_comercial/tiendas/productos/js/actualizar_stock.js
@@ -2,7 +2,16 @@ document.addEventListener("DOMContentLoaded", function() {
     const switchCheckbox = document.querySelectorAll(".switch-input");
 
     switchCheckbox.forEach(checkbox => {
-        checkbox.addEventListener("change")
+        checkbox.addEventListener("change", function() {
+            if (this.checked) {
+                updateStock
+            }
+        })
     })
 
 });
+
+function updateStock(idProducto, stock)
+{
+    fetch('https://ustoreapi.azurewebsites.net/api/Productos/CreateProducto?idProducto')
+}
