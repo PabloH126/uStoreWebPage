60786dbce1e66f0406edf479e1f73fceae43b8d3
PabloH126 <pabo120603@outlook.com>
2023-08-30 19:09:29 -0600

    perfil producto 3.4

--- restringido/centro_comercial/tiendas/productos/lista_productos.php
+++ restringido/centro_comercial/tiendas/productos/lista_productos.php
@@ -1,7 +1,30 @@
  <?php 
 	session_start();
 	require '../../../security.php';
-	require 'datosProducto.php';
+	
+	$ch = curl_init();
+
+	curl_setopt($ch, CURLOPT_URL, "https://ustoreapi.azurewebsites.net/api/Productos/GetProductos?idTienda=" . $_GET['id']);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		'Authorization: Bearer ' . $_COOKIE['SessionToken']
+	));
+
+	$response = curl_exec($ch);
+
+	if ($response === false) {
+		echo 'Error: ' . curl_error($ch);
+	} else {
+		$httpStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	}
+
+	if ($httpStatusCode == 400) {
+		$productosError = "Error al intentar recuperar los productos. Codigo de respuesta: " . $httpStatusCode;
+	}
+
+	$productos = json_decode($response, true);
+	curl_close($ch);
 	
 ?>
 <!DOCTYPE html>
