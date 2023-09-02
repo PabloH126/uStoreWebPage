6d2707dd8ea6b65ddad2452e7b0c1e59dc1e70b7
PabloH126 <pabo120603@outlook.com>
2023-09-02 13:46:36 -0600

    diseño solicitudes de apartados 4.0.1

--- restringido/centro_comercial/templates/template.styles.php
+++ restringido/centro_comercial/templates/template.styles.php
@@ -6,3 +6,6 @@
 <!--GOOGLE ICONS-->
 <!--ADD-->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+
+<!-- ICONS -->
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
--- restringido/templates/template.styles.php
+++ restringido/templates/template.styles.php
@@ -7,5 +7,4 @@
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Golos+Text&display=swap" rel="stylesheet">
 	
-	<!-- ICONS -->
-	<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+	
