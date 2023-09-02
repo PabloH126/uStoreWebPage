9082c0d02734f0aed1a5cc423010f24a47757bdd
PabloH126 <pabo120603@outlook.com>
2023-09-02 12:41:56 -0600

    diseño solicitudes de apartado 2.2.4

--- restringido/centro_comercial/solicitudes_apartado/solicitudes_apartado.php
+++ restringido/centro_comercial/solicitudes_apartado/solicitudes_apartado.php
@@ -42,7 +42,7 @@
 	<div class="content">
 		<div class="title">
 			<h1>Solicitudes de apartado</h1>
-			<a href="" class="bttn_cambio_seccion">ver solicitudes activas</a>
+			<a href="" class="bttn_cambio_seccion">Ver solicitudes activas</a>
 		</div>
 
 		<div>
