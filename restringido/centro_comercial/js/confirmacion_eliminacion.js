abe32b4c867b019af864e044bd4fe72e5d3f14dc
PabloH126 <pabo120603@outlook.com>
2023-09-05 20:44:34 -0600

    eliminacion de productos/tiendas modificados v1.2

--- restringido/centro_comercial/js/confirmacion_eliminacion.js
+++ restringido/centro_comercial/js/confirmacion_eliminacion.js
@@ -12,7 +12,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
             modal.innerHTML = `
                 <div class="modal-content">
-                    <p>¿Estás seguro de que deseas eliminar esta tienda? También se eliminarán todos los productos asociados a ella.</p>
+                    <p>¿Estás seguro de que deseas eliminar esta tienda?</p>
+                    <p>También se eliminarán todos los productos asociados a ella.</p>
                     <div class="modal-buttons">
                         <button class="modal-accept">Aceptar</button>
                         <button class="modal-cancel">Cancelar</button>
