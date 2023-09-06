c96d89834e7df096bf394ed5de4c9d82f407d29f
PabloH126 <pabo120603@outlook.com>
2023-09-05 20:19:30 -0600

    eliminacion de productos/tiendas modificados v1.1.3

--- restringido/centro_comercial/tiendas/productos/js/eliminacion_productos.js
+++ restringido/centro_comercial/tiendas/productos/js/eliminacion_productos.js
@@ -47,7 +47,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     if(data.status === 'success')
                     {
                         closeModal();
-                        const url = "https://ustoree.azurewebsites.net/restringido/centro_comercial/tiendas/productos/lista_productos.php?id=" + data.idTienda;
+                        const url = "lista_productos.php?id=" + data.idTienda;
                         hideNotification();
                         showNotification("Producto eliminado exitosamente");
                         setTimeout(() => {
