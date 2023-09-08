c90e37e50353652800c86e3072cb0290979f790d
PabloH126 <pabo120603@outlook.com>
2023-09-08 15:47:17 -0600

    solicitudes de apartado 2.2.3.1

--- restringido/centro_comercial/tiendas/productos/perfil_producto.php
+++ restringido/centro_comercial/tiendas/productos/perfil_producto.php
@@ -2,9 +2,7 @@
 session_start();
 require '../../../security.php';
 require 'datosProducto.php';
-include 'datos_session_productos.php';
 var_dump($_SESSION);
-echo $idTienda . ' ' . $idMall . ' ' . $idUser;
 $_SESSION['idProducto'] = $_GET['id'];
 ?>
 <!DOCTYPE html>
