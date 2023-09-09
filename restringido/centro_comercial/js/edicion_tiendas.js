780b9f04f143b93773d44e4db02ab093419ef4c0
PabloH126 <pabo120603@outlook.com>
2023-09-09 16:41:48 -0600

    eliminacion de imagenes edicion tienda v1.1.6

--- restringido/centro_comercial/js/edicion_tiendas.js
+++ restringido/centro_comercial/js/edicion_tiendas.js
@@ -501,10 +501,17 @@ function validacionHorarios() {
 
 function validacionBanner() {
     let img1 = document.getElementById("fileInput1");
+    let imgId1 = document.getElementById("idImagen1");
+    
     let img2 = document.getElementById("fileInput2");
+    let imgId2 = document.getElementById("idImagen2");
+
     let img3 = document.getElementById("fileInput3");
+    let imgId3 = document.getElementById("idImagen3");
+
     let logoTienda = document.getElementById("logoTienda");
-    if (!img1.files.length && !img2.files.length && !img3.files.length && img1.files.length > 0 && img2.files.length > 0 && img3.files.length > 0) {
+
+    if (!img1.files.length && !img2.files.length && !img3.files.length && imgId1.value == "0" && imgId2 == "0" && imgId3 == "0") {
         showNotificationError("Se debe subir al menos una imagen para el banner de la tienda");
         return false;
     }
