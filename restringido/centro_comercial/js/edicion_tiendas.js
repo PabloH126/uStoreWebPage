bba3fa7a2b9ba0716b7cb7b59ebb8599ae3cfa11
PabloH126 <pabo120603@outlook.com>
2023-09-09 21:10:28 -0600

    eliminacion de imagenes edicion tienda v1.6

--- restringido/centro_comercial/js/edicion_tiendas.js
+++ restringido/centro_comercial/js/edicion_tiendas.js
@@ -58,6 +58,8 @@ document.addEventListener('DOMContentLoaded', function () {
                             setTimeout(() => {
                                 hideNotification();
                             }, 1500);
+                            window.location.hash = "#item-5";
+                            window.location.reload();
                         }
                         else
                         {
