6f1713845a70ce3d37f9d5f2f68632ec826fab3c
PabloH126 <pabo120603@outlook.com>
2023-09-10 23:28:47 -0600

    prueba promociones con API v1.0

--- restringido/centro_comercial/promociones/promociones.php
+++ restringido/centro_comercial/promociones/promociones.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 require '../../security.php';
-/*
+
 	$ch = curl_init();
 
-	curl_setopt($ch, CURLOPT_URL, "https://ustoreapi.azurewebsites.net/api/Tiendas/GetTiendas?idCentroComercial=" . $_SESSION['idMall']);
+	curl_setopt($ch, CURLOPT_URL, "https://ustoreapi.azurewebsites.net/api/Publicaciones/GetPublicacionesRecientes?idMall=" . $_SESSION['idMall']);
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
@@ -22,9 +22,9 @@ require '../../security.php';
 	if ($httpStatusCode == 400) {
 		$tiendasError = "Error al intentar recuperar las tiendas. Codigo de respuesta: " . $httpStatusCode;
 	}
-	$tiendas = json_decode($response, true);
+	$publicaciones = json_decode($response, true);
 	curl_close($ch);
-	*/
+	
 ?>
 <!DOCTYPE html>
 <html>
@@ -42,30 +42,25 @@ require '../../security.php';
 
 	<div class="content">
         <div class="content-ofertas">
-            
-            <div class="space-publication">
+            <?php
+			foreach($publicaciones as $publicacion)
+			{
+			?>
+			<div class="space-publication">
                 <div class="header-publication">
-                    <img src="https://i.blogs.es/c2d211/minato/840_560.jpeg" alt="mi poderosisimo minato">
-                    <label class="name">mi poderosisimo minato como no</label>
+                    <img src="<?php echo $publicacion['logoTienda']; ?>" alt="logoTienda">
+                    <label class="name"><?php echo $publicacion['nombreTienda']; ?></label>
                 </div>
                 <div class="body-publication">
-                    <p>mi poderosisimo minato ❤ </p>
-                    <img src="https://tierragamer.com/wp-content/uploads/2023/07/MinatoRasenganTierraGamer.png"
-                        alt="otro poderosisimo minato como no">
+                    <p><?php echo $publicacion['contenido']; ?></p>
+                    <img src="<?php echo $publicacion['imagen']; ?>"
+                        alt="imagenPublicacion">
                 </div>
             </div>
             <hr>
-            <div class="space-publication">
-                <div class="header-publication">
-                    <img src="https://i.blogs.es/c2d211/minato/840_560.jpeg" alt="mi poderosisimo minato">
-                    <label class="name">mi poderosisimo minato como no</label>
-                </div>
-                <div class="body-publication">
-                    <p>mi poderosisimo minato ❤ </p>
-                    <img src="https://tierragamer.com/wp-content/uploads/2023/07/MinatoRasenganTierraGamer.png"
-                        alt="otro poderosisimo minato como no">
-                </div>
-            </div>
+			<?php
+			}
+			?>
         </div>
 		<div class="crear-publicacion">
 			<a title="Crear publicación" href="crear_publicacion.php">
