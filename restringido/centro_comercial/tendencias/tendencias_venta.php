990df0f91eab0ab3b1b5072a4e497430313e5a3a
PabloH126 <pabo120603@outlook.com>
2023-09-11 17:18:01 -0600

    tendencias de venta aside 2.2.2

--- restringido/centro_comercial/tendencias/tendencias_venta.php
+++ restringido/centro_comercial/tendencias/tendencias_venta.php
@@ -80,7 +80,7 @@ require '../../security.php';
 
 				<li class="list_item">
                     <div class="list_button list_button--click">
-						<i class='bx bxs-time-five'></i>
+						<i class='bx bxs-category' ></i>
                         <p>Tiempo</p>
                         <i class='bx bx-chevron-right'></i>
                     </div>
