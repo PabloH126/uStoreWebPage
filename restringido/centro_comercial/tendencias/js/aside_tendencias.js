7ff6c1ff9e660e489c14d215dcac8513dfd11512
PabloH126 <pabo120603@outlook.com>
2023-09-12 13:31:50 -0600

    tendencias de venta aside 3.1.1

--- restringido/centro_comercial/tendencias/js/aside_tendencias.js
+++ restringido/centro_comercial/tendencias/js/aside_tendencias.js
@@ -1,8 +1,16 @@
 let listElements = document.querySelectorAll('.list_button--click');
 
 listElements.forEach(listElement => {
+
+    //al dar click
     listElement.addEventListener('click', ()=>{
         
+        //agrega la clase arrow
         listElement.classList.toggle('arrow');
+
+        //y muestra el submenu
+        let height = 0;
+        let menu = listElement.nextElementSibling;
+        console.log(menu);
     })
 });
