57b45ec43d94c542287f854f92c29065a03dcb6d
PabloH126 <pabo120603@outlook.com>
2023-09-12 21:09:40 -0600

    Prueba grafica tendencias venta v1.1

--- restringido/centro_comercial/tendencias/js/grafica.js
+++ restringido/centro_comercial/tendencias/js/grafica.js
@@ -1,34 +1,36 @@
 var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'];
 var ventas = [3000, 4000, 3200, 5000, 8000, 3900];
 
-var ctx = document.getElementById('grafica').getContext('2d');
-var grafica = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: meses,
-        datasets:  [{
-            label: 'Ventas',
-            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-            borderColor: 'rgba(75, 192, 192, 1)',
-            data: ventas,
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
+document.addEventListener('DOMContentLoaded', function() {
+    var ctx = document.getElementById('grafica').getContext('2d');
+    var grafica = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: meses,
+            datasets:  [{
+                label: 'Ventas',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                data: ventas,
+                borderWidth: 1
+            }]
         },
-        animation: {
-            duration: 1000,
-            easing: 'easeOutBounce',
-            onProgress: function(animation) {
-
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
             },
-            onComplete: function(animation) {
+            animation: {
+                duration: 1000,
+                easing: 'easeOutBounce',
+                onProgress: function(animation) {
 
+                },
+                onComplete: function(animation) {
+
+                }
             }
         }
-    }
+    });
 });
