ec058ee894bf3ff2c58f5ffc87012f38746795f3
PabloH126 <pabo120603@outlook.com>
2023-09-18 21:00:56 -0600

    prueba solicitudes activas v1.3.4

--- restringido/centro_comercial/solicitudes_apartado/solicitudes_activas.php
+++ restringido/centro_comercial/solicitudes_apartado/solicitudes_activas.php
@@ -133,8 +133,14 @@
 					$tiempoActual = new DateTime('now', new DateTimeZone('Etc/GMT+6'));
 					foreach ($solicitudes as $solicitud)
 					{
-						$tiempoVencimiento = DateTime::createFromFormat('Y-m-d H:i:s.v', $solicitud['fechaVencimiento'], new DateTimeZone('UTC'));
+						//$tiempoVencimiento = DateTime::createFromFormat('Y-m-d H:i:s.v', $solicitud['fechaVencimiento'], new DateTimeZone('UTC'));
 						//$tiempoVencimiento->setTimezone(new DateTimeZone('America/Belize'));
+						$tiempoVencimiento = DateTime::createFromFormat('Y-m-d H:i:s.v', $solicitud['fechaVencimiento'], new DateTimeZone('UTC'));
+
+						$errors = DateTime::getLastErrors();
+						if ($errors['warning_count'] > 0 || $errors['error_count'] > 0) {
+							print_r($errors);
+						}
 				?>
 						<div class="item">
 							<img src="<?php echo $solicitud['imageProducto'];?>" alt="">
