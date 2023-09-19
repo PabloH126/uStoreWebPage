778f23c074d1a6453fb78d67bc097ab3eee2c311
PabloH126 <pabo120603@outlook.com>
2023-09-19 14:36:36 -0600

    prueba publicaciones

--- restringido/centro_comercial/promociones/promociones.php
+++ restringido/centro_comercial/promociones/promociones.php
@@ -2,9 +2,34 @@
 session_start();
 require '../../security.php';
 
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://ustoreapi.azurewebsites.net/api/Tiendas/GetTiendas?idCentroComercial=" . $_SESSION['idMall']);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+	'Authorization: Bearer ' . $_COOKIE['SessionToken']
+));
+
+$response = curl_exec($ch);
+
+if ($response === false) {
+	echo 'Error: ' . curl_error($ch);
+} else {
+	$httpStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+}
+
+if ($httpStatusCode == 400) {
+	$tiendasError = "Error al intentar recuperar las tiendas. Codigo de respuesta: " . $httpStatusCode;
+}
+$tiendas = json_decode($response, true);
+curl_close($ch);
+
+if (isset($_GET['id']))
+{
 	$ch = curl_init();
 
-	curl_setopt($ch, CURLOPT_URL, "https://ustoreapi.azurewebsites.net/api/Publicaciones/GetPublicacionesRecientes?idMall=" . $_SESSION['idMall']);
+	curl_setopt($ch, CURLOPT_URL, "https://ustoreapi.azurewebsites.net/api/Publicaciones/GetPublicacionesRecientes?idTienda=" . $_GET['id']);
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
@@ -24,7 +49,7 @@ require '../../security.php';
 	}
 	$publicaciones = json_decode($response, true);
 	curl_close($ch);
-	
+}
 ?>
 <!DOCTYPE html>
 <html>
