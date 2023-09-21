f1e191b3ac24df90acd199f7e74d44fda88b149a
PabloH126 <pabo120603@outlook.com>
2023-09-20 19:51:05 -0600

    prueba solicitudes SignalR v1.6

--- restringido/centro_comercial/solicitudes_apartado/js/updateSolicitud.js
+++ restringido/centro_comercial/solicitudes_apartado/js/updateSolicitud.js
@@ -24,6 +24,7 @@ connection.on("RecieveSolicitudes", function (solicitudes) {
         nota.style.display = "";
         spanSolicitudes.style.display = "none";
         solicitudesContainer.appendChild(solicitudElement);
+        solicitudesContainer.offsetHeight;
     });
 });
 
