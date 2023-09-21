97a12b8a4fb71032218ef8cea25d28e4ea3c12d9
PabloH126 <pabo120603@outlook.com>
2023-09-20 22:01:40 -0600

    hover menu

--- restringido/centro_comercial/solicitudes_apartado/solicitudes_apartado.php
+++ restringido/centro_comercial/solicitudes_apartado/solicitudes_apartado.php
@@ -25,6 +25,30 @@
 	$tiendas = json_decode($response, true);
 	curl_close($ch);
 
+
+	$ch = curl_init();
+
+	curl_setopt($ch, CURLOPT_URL, "https://ustoreapi.azurewebsites.net/api/Apartados/GetNumeroSolicitudes");
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		'Authorization: Bearer ' . $_COOKIE['SessionToken']
+	));
+	
+	$response = curl_exec($ch);
+	
+	if ($response === false) {
+		echo 'Error: ' . curl_error($ch);
+	} else {
+		$httpStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	}
+	
+	if ($httpStatusCode == 400) {
+		$numeroSolicitudes = "Error al intentar recuperar las tiendas. Codigo de respuesta: " . $httpStatusCode;
+	}
+	$tiendas = json_decode($response, true);
+	curl_close($ch);
+
 	if (isset($_GET['id']))
 	{
 		$ch = curl_init();
