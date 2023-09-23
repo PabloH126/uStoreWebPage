5306ac894cb7be162707ccc4ed9b92cc94ad02ef
PabloH126 <pabo120603@outlook.com>
2023-09-22 21:01:22 -0600

    aside chats 1.4.8

--- restringido/centro_comercial/chats/templates/template.aside_chats.php
+++ restringido/centro_comercial/chats/templates/template.aside_chats.php
@@ -8,8 +8,7 @@
         </div>
 
         <div class="search-aside-chat">
-          <script async src="https://cse.google.com/cse.js?cx=16f3c4e0fe5e5438c"></script>
-          <div class="gcse-search"></div>
+          
         </div>
       </div>
 
