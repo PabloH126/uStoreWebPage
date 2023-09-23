e831b3b72c6b1af2956f3e89eefc4c0e6390adcb
PabloH126 <pabo120603@outlook.com>
2023-09-22 18:59:34 -0600

    aside chats 1.1.1

--- restringido/centro_comercial/chats/templates/template.secc_chats.php
+++ restringido/centro_comercial/chats/templates/template.secc_chats.php
@@ -1,2 +1,3 @@
 <link rel="stylesheet" type="text/css" href="css/secc_chats.css">
 <link rel="stylesheet" type="text/css" href="../css/aside_general.css">
+<link rel="stylesheet" type="text/css" href="css/aside_chats.css">
