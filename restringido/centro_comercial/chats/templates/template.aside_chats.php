8ac8cb91a181c7802fca025bb0b51418963d69a6
PabloH126 <pabo120603@outlook.com>
2023-09-23 14:31:35 -0600

    buscar chat 1.3.4

--- restringido/centro_comercial/chats/templates/template.aside_chats.php
+++ restringido/centro_comercial/chats/templates/template.aside_chats.php
@@ -13,20 +13,10 @@
         <form method="get" id="buscarform">
             <input type="text" id="s" value="" placeholder="Buscar"  />
             <input class="button" type="submit" value="" />
-            
+            <i class='bx bx-search search'>
             <i class="search"></i>
         </form>
       </div>
-
-<!--
-        <div class="search-aside-chat">
-            <form>
-                <!--<label for="buscador"><i class='bx bx-search'></i></label>-->
-              <!--  <input type="search" id="buscador"></input>
-            </form>
-        </div>
-      </div>
--->
 
       <div class="body-aside">
 
