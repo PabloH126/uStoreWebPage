242ccb525a0eaaf11b464190493e9154d1ac3d09
PabloH126 <pabo120603@outlook.com>
2023-09-22 20:31:47 -0600

    aside chats 1.4.3

--- restringido/centro_comercial/chats/templates/template.aside_chats.php
+++ restringido/centro_comercial/chats/templates/template.aside_chats.php
@@ -2,13 +2,11 @@
     <ul class="list">
       <div class="header_aside">
         <div class="aside_options_chat">
-          <div class="options_aside">
-            <button>Usuarios</button>
-          </div>
+            <button class="options_aside">Usuarios</button>
           <hr>
-          <div class="options_aside">
-            <button>Gerentes</button>
-          </div>
+          <!-- <div class="options_aside"> -->
+            <button class="options_aside">Gerentes</button>
+          <!-- </div> -->
         </div>
 
         <div class="search-aside-chat">
