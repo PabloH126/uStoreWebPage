4b66ff92dc4197bef36b062ed8ae3c90e2327aab
PabloH126 <pabo120603@outlook.com>
2023-09-24 14:13:46 -0600

    aside tendencias 3.2

--- restringido/centro_comercial/tendencias/js/aside_tendencias.js
+++ restringido/centro_comercial/tendencias/js/aside_tendencias.js
@@ -2,6 +2,10 @@ let listElements = document.querySelectorAll('.list_button--click');
 
 listElements.forEach(listElement => {
 
+    if (listElement.classList.contains('categorias')) {
+        menu.style.height = 37+"vh";
+    }
+
     //al dar click
     listElement.addEventListener('click', ()=>{
         
@@ -17,11 +21,6 @@ listElements.forEach(listElement => {
         }
 
         menu.style.height = height+"px";
-
-        if (listElement.classList.contains('categorias')) {
-            menu.style.height = 37+"vh";
-        }
-
         
     })
 });
