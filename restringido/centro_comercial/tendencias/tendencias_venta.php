5e401f5cd38a810b4ce9caa5bc916a53373a43e0
PabloH126 <pabo120603@outlook.com>
2023-09-24 16:06:26 -0600

    correcion tendencias venta 1

--- restringido/centro_comercial/tendencias/tendencias_venta.php
+++ restringido/centro_comercial/tendencias/tendencias_venta.php
@@ -94,39 +94,39 @@ curl_close($ch);
             </ul>
 		</aside>
 
-		<div class="">
-
-		</div>
-		<div class="title">
-			<div>
-				<h1>Tendencias de venta</h1>
-			</div>
-			<div id="filterList">
-				<span class="material-symbols-outlined icon-filter" id="menu-icon">filter_list</span>
-				<div id="sub-menu">
-					<div class="menu-option"><a id="MayorMenor">Mayor a Menor</a></div>
-					<div class="menu-option"><a id="MenorMayor">Menor a Mayor</a></div>
-					<div class="menu-option"><a id="A-Z">Nombre: A-Z</a></div>
-					<div class="menu-option"><a id="Z-A">Nombre: Z-A</a></div>
+		<div class="grafic-area">
+			<div class="title">
+				<div>
+					<h1>Tendencias de venta</h1>
+				</div>
+				<div id="filterList">
+					<span class="material-symbols-outlined icon-filter" id="menu-icon">filter_list</span>
+					<div id="sub-menu">
+						<div class="menu-option"><a id="MayorMenor">Mayor a Menor</a></div>
+						<div class="menu-option"><a id="MenorMayor">Menor a Mayor</a></div>
+						<div class="menu-option"><a id="A-Z">Nombre: A-Z</a></div>
+						<div class="menu-option"><a id="Z-A">Nombre: Z-A</a></div>
+					</div>
 				</div>
 			</div>
-		</div>
 
-		<div class="body">
-			<canvas id="grafica"></canvas>
+			<div class="body">
+				<canvas id="grafica"></canvas>
 
-			<div class="crear-publicacion" id="btnCrearPubli">				
-				<a title="Descargar">
-					<i class='bx bxs-download' id="menu-icon2"></i>
-				</a>
-			</div>	
-			
-			<div id="sub-menu2">
-				<div class="menu-option"><a id="downloadPDF">PDF</a></div>
-				<div class="menu-option"><a id="downloadImage">PNG</a></div>
+				<div class="crear-publicacion" id="btnCrearPubli">				
+					<a title="Descargar">
+						<i class='bx bxs-download' id="menu-icon2"></i>
+					</a>
+				</div>	
+				
+				<div id="sub-menu2">
+					<div class="menu-option"><a id="downloadPDF">PDF</a></div>
+					<div class="menu-option"><a id="downloadImage">PNG</a></div>
+				</div>
 			</div>
+			<span id="span-seleccion-tienda">Selecciona una opcion de filtro</span>
 		</div>
-		<span id="span-seleccion-tienda">Selecciona una opcion de filtro</span>
+		
 	</div>
 	<?php require("templates/template.scripts_tendencias.php") ?>
 </body>
