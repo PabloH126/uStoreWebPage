d2ea26406f60176478e6943418d47f641b758002
PabloH126 <pabo120603@outlook.com>
2023-09-24 21:13:08 -0600

    chats 3.2.6

--- restringido/centro_comercial/chats/chats.php
+++ restringido/centro_comercial/chats/chats.php
@@ -56,7 +56,6 @@ require '../../security.php';
 					<div>
 						<label for="submit_message"><i class='bx bx-send bx-flip-vertical'></i></label>
 						<input type="submit" id="submit_message">
-						<i class='bx bxs-send'></i>
 					</div>
 				</form>
 			</div>
