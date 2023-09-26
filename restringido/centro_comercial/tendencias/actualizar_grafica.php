b36bb6aa0874aa886bcc188c6b7800d3b5218c96
PabloH126 <pabo120603@outlook.com>
2023-09-25 19:54:27 -0600

    datos grafica tendencias v1.2.5

--- restringido/centro_comercial/tendencias/actualizar_grafica.php
+++ restringido/centro_comercial/tendencias/actualizar_grafica.php
@@ -12,14 +12,11 @@
         "periodoTiempo" => /*(isset($_POST['periodoTiempo']) ? $_POST['periodoTiempo'] : */"semanal"//)
     ];
 
-    echo json_encode(['status' => $data]);
-        exit;
-
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, "https://ustoreapi.azurewebsites.net/api/TendenciasVenta/GetTendencias");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
         'Authorization: Bearer ' . $_COOKIE['SessionToken'],
         'Content-Type: application/json'
