afa199fb1d905196223d79d4a16d9d43c1e6faa1
PabloH126 <pabo120603@outlook.com>
2023-09-26 21:49:14 -0600

    datos grafica tendencias v1.6.5

--- restringido/centro_comercial/chats/chats.php
+++ restringido/centro_comercial/chats/chats.php
@@ -52,8 +52,7 @@ require '../../security.php';
 
 				<div class="outgoing-msg">
 					<div class="outgoing-chats-msg">
-						<p class="outgoing-box-msg">Hi riya , Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo nobis deleniti earum magni recusandae assumenda.
-						</p>
+						<p class="outgoing-box-msg">Hi riya , Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo nobis deleniti earum magni recusandae assumenda.</p>
 						<p class="outgoing-box-msg">Lorem ipsum dolor sit amet consectetur.</p>
 						<span class="time">18:30 PM | July 24 </span>
 					</div>
