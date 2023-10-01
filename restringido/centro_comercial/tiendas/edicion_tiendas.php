b7a570265f4780e43a263be0cb608fa907be7801
PabloH126 <pabo120603@outlook.com>
2023-09-30 19:39:04 -0600

    prueba de animacion 1.0.1

--- restringido/centro_comercial/tiendas/edicion_tiendas.php
+++ restringido/centro_comercial/tiendas/edicion_tiendas.php
@@ -308,7 +308,7 @@ function CategoriasSelect($categoriasDisponibles, $categoriasTiendaId)
                         </div>
                         <div class="bttn" id="send">
                             <!-- <button type="submit">Guardar</button> -->
-                            <button class="loadingContainer loading">
+                            <button class="loading">
                                 <div class="ball1"></div>
                                 <div class="ball2"></div>
                                 <div class="ball3"></div>
