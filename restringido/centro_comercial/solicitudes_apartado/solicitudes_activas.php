931d32df31a078b491fc92b4c57170ca810ee13e
PabloH126 <pabo120603@outlook.com>
2023-10-01 16:51:07 -0600

    correcion solicitudes activas time v1.5.8

--- restringido/centro_comercial/solicitudes_apartado/solicitudes_activas.php
+++ restringido/centro_comercial/solicitudes_apartado/solicitudes_activas.php
@@ -154,7 +154,6 @@
 							$intervalo = "0";
 						}
 						$tiempoVencimiento = $tiempoVencimiento->format('Y-m-d H:i:s');
-						echo $intervalo;
 				?>
 						<div class="item solicitudesItem">
 							<img src="<?php echo $solicitud['imageProducto'];?>" alt="">
