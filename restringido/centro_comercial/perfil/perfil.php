0c55a47dd9b8101a811b71c41371ba51abf71283
PabloH126 <pabo120603@outlook.com>
2023-10-01 12:28:24 -0600

    perfil 2

---
+++ restringido/centro_comercial/perfil/perfil.php
@@ -0,0 +1,76 @@
+<?php
+session_start();
+require '../../security.php';
+/*
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://ustoreapi.azurewebsites.net/api/Tiendas/GetTiendas?idCentroComercial=" . $_SESSION['idMall']);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+	'Authorization: Bearer ' . $_COOKIE['SessionToken']
+));
+
+$response = curl_exec($ch);
+
+if ($response === false) {
+	echo 'Error: ' . curl_error($ch);
+} else {
+	$httpStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+}
+
+if ($httpStatusCode == 400) {
+	$tiendasError = "Error al intentar recuperar las tiendas. Codigo de respuesta: " . $httpStatusCode;
+}
+$tiendas = json_decode($response, true);
+curl_close($ch);
+
+if (isset($_GET['id']))
+{
+	$ch = curl_init();
+
+	curl_setopt($ch, CURLOPT_URL, "https://ustoreapi.azurewebsites.net/api/Publicaciones/GetPublicacionesRecientes?idTienda=" . $_GET['id']);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		'Authorization: Bearer ' . $_COOKIE['SessionToken']
+	));
+	
+	$response = curl_exec($ch);
+	
+	if ($response === false) {
+		echo 'Error: ' . curl_error($ch);
+	} else {
+		$httpStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	}
+	
+	if ($httpStatusCode == 400) {
+		$publicacionesError = "Error al intentar recuperar las publicaciones. Codigo de respuesta: " . $httpStatusCode;
+	}
+	$publicaciones = json_decode($response, true);
+	curl_close($ch);
+}
+*/
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Publicación de promociones</title>
+	<?php require("../templates/template.styles.php") ?>
+	<?php require("templates/template.secc_chat.php") ?>
+</head>
+
+<body>
+	<?php require("../templates/template.menu.php") ?>
+
+	<div class="content">
+		
+
+        
+		
+	</div>
+	<script src="../js/menu_desplegable.js"></script>
+</body>
+</html>
