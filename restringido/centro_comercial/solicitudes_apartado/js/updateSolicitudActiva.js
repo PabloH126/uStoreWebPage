84061abf7355f2866417e363a7998b9536ff8887
PabloH126 <pabo120603@outlook.com>
2023-10-01 16:12:04 -0600

    correcion solicitudes activas time v1.5.1

--- restringido/centro_comercial/solicitudes_apartado/js/updateSolicitudActiva.js
+++ restringido/centro_comercial/solicitudes_apartado/js/updateSolicitudActiva.js
@@ -21,6 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
     
     timers.forEach(timer => {
         var time = timer.getAttribute("data-time");
+        console.log(time);
         if (time !== "0")
         {
             time = time.split(":");
@@ -70,7 +71,7 @@ document.addEventListener("DOMContentLoaded", function () {
             {
                 timerText.push(`${segundos.toString().padStart(2, '0')}s`);
             }
-            
+
             timer.textContent = timerText.join(": ");
         }, 1000);
     });
