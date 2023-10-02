dd0e01944c3ce1874150b129bbe590e50637e860
PabloH126 <pabo120603@outlook.com>
2023-10-02 16:56:16 -0600

    aside perfil 1.2.3

--- restringido/centro_comercial/perfil/perfil.php
+++ restringido/centro_comercial/perfil/perfil.php
@@ -9,7 +9,8 @@ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 	'Authorization: Bearer ' . $_COOKIE['SessionToken']
-));
+)
+);
 
 $response = curl_exec($ch);
 
@@ -25,8 +26,7 @@ if ($httpStatusCode == 400) {
 $tiendas = json_decode($response, true);
 curl_close($ch);
 
-if (isset($_GET['id']))
-{
+if (isset($_GET['id'])) {
 	$ch = curl_init();
 
 	curl_setopt($ch, CURLOPT_URL, "https://ustoreapi.azurewebsites.net/api/Publicaciones/GetPublicacionesRecientes?idTienda=" . $_GET['id']);
@@ -34,16 +34,17 @@ if (isset($_GET['id']))
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 		'Authorization: Bearer ' . $_COOKIE['SessionToken']
-	));
-	
+	)
+	);
+
 	$response = curl_exec($ch);
-	
+
 	if ($response === false) {
 		echo 'Error: ' . curl_error($ch);
 	} else {
 		$httpStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 	}
-	
+
 	if ($httpStatusCode == 400) {
 		$publicacionesError = "Error al intentar recuperar las publicaciones. Codigo de respuesta: " . $httpStatusCode;
 	}
@@ -66,11 +67,12 @@ if (isset($_GET['id']))
 	<?php require("../templates/template.menu.php") ?>
 
 	<div class="content">
-        <div class="header_profile">
+		<div class="header_profile">
 			<div class="header_info_profile">
 				<div class="info_img_profile">
 					<div class="img_profile">
-						<img src="https://i.pinimg.com/564x/4f/cd/0d/4fcd0d0480073175756ac628a1cf959e.jpg" alt="imagen de perfil">
+						<img src="https://i.pinimg.com/564x/4f/cd/0d/4fcd0d0480073175756ac628a1cf959e.jpg"
+							alt="imagen de perfil">
 					</div>
 					<div class="info_profil">
 						<p>Nombre</p>
@@ -83,19 +85,18 @@ if (isset($_GET['id']))
 				<div class="log-out">
 					<img src="https://ustoree.azurewebsites.net/img/log_out.png" alt="Cerrar sesión">
 				</div>
-				
+
 			</div>
 			<div class="top_menu_profile">
 				<button class="graficas_option">Gráficas</button>
 				<button class="gerentes_option">Gerentes</button>
 			</div>
-        </div>
-        <div class="aside_profile">
-			<aside>
-
-			</aside>
-        </div>
+		</div>
+		<div class="aside_profile">
+			<?php require("../templates/template.aside.php") ?>
+		</div>
 	</div>
 	<script src="../js/menu_desplegable.js"></script>
 </body>
+
 </html>
