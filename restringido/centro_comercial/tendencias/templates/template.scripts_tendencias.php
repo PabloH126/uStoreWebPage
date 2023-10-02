51394afe5f4e24597f9c4c5c8b2b232541ab536a
PabloH126 <pabo120603@outlook.com>
2023-10-02 17:00:29 -0600

    aside perfil 2

--- restringido/centro_comercial/tendencias/templates/template.scripts_tendencias.php
+++ restringido/centro_comercial/tendencias/templates/template.scripts_tendencias.php
@@ -1,6 +1,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.3.2/html2canvas.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script src="js/aside_tendencias.js"></script>
+<script src="../js/menu_aside.js"></script>
 <script src="js/grafica.js"></script>
 <script src="../js/menu_desplegable.js"></script>
