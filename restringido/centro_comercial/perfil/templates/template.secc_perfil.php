861e6a28ad8492083eaf7520d0d0a37fb5e39ca7
PabloH126 <pabo120603@outlook.com>
2023-10-03 13:35:46 -0600

    perfil body 3.0.4

--- restringido/centro_comercial/perfil/templates/template.secc_perfil.php
+++ restringido/centro_comercial/perfil/templates/template.secc_perfil.php
@@ -3,3 +3,4 @@
 <link rel="stylesheet" type="text/css" href="css/secc_profile.css">
 <link rel="stylesheet" type="text/css" href="css/header_profile.css">
 <link rel="stylesheet" type="text/css" href="css/aside_profile.css">
+<link rel="stylesheet" type="text/css" href="css/perfil_graficas.css">
