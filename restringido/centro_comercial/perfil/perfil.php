8fcbb23082446c7e7eb32024bb6554f3ae38c1ca
PabloH126 <pabo120603@outlook.com>
2023-10-03 14:31:34 -0600

    perfil body 3.0.6

--- restringido/centro_comercial/perfil/perfil.php
+++ restringido/centro_comercial/perfil/perfil.php
@@ -105,30 +105,33 @@ curl_close($ch);
 			<?php require("../templates/template.aside.php") ?>
 			<div class="body">
 				<canvas id="grafica"></canvas>	
+				<span id="span-seleccion-tienda">Selecciona una opción de filtro</span>
 			</div>
 
-			<div>
-			<div id="sub-menu2">
-					<div class="menu-option"><a id="downloadPDF">PDF</a></div>
-					<div class="menu-option"><a id="downloadImage">PNG</a></div>
+			<div class="floating_bttns">
+				
+				<div id="filterList">
+					<span class="material-symbols-outlined icon-filter" id="menu-icon">filter_list</span>
+					<div id="sub-menu">
+						<div class="menu-option"><a id="MayorMenor">Mayor a Menor</a></div>
+						<div class="menu-option"><a id="MenorMayor">Menor a Mayor</a></div>
+						<div class="menu-option"><a id="A-Z">Nombre: A-Z</a></div>
+						<div class="menu-option"><a id="Z-A">Nombre: Z-A</a></div>
+					</div>
 				</div>
-			<div class="crear-publicacion" id="btnCrearPubli">				
-				<a title="Descargar">
-					<i class='bx bxs-download' id="menu-icon2"></i>
-				</a>
-			</div>	
-			<div id="filterList">
-				<span class="material-symbols-outlined icon-filter" id="menu-icon">filter_list</span>
-				<div id="sub-menu">
-					<div class="menu-option"><a id="MayorMenor">Mayor a Menor</a></div>
-					<div class="menu-option"><a id="MenorMayor">Menor a Mayor</a></div>
-					<div class="menu-option"><a id="A-Z">Nombre: A-Z</a></div>
-					<div class="menu-option"><a id="Z-A">Nombre: Z-A</a></div>
-				</div>
-				</div>
+
+				<div class="crear-publicacion" id="btnCrearPubli">				
+					<a title="Descargar">
+						<i class='bx bxs-download' id="menu-icon2"></i>
+					</a>
+					<div id="sub-menu2">
+						<div class="menu-option"><a id="downloadPDF">PDF</a></div>
+						<div class="menu-option"><a id="downloadImage">PNG</a></div>
+					</div>
+				</div>	
 			</div>
 			
-			<span id="span-seleccion-tienda">Selecciona una opción de filtro</span>
+			
 		</div>
 		<?php
 		}
