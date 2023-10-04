f0c1a85afd5175bb73725f6dfc432af979287836
PabloH126 <pabo120603@outlook.com>
2023-10-03 21:29:44 -0600

    filtro imagen de perfil 4.1.1

--- restringido/centro_comercial/tendencias/js/grafica.js
+++ restringido/centro_comercial/tendencias/js/grafica.js
@@ -181,7 +181,7 @@ function DesactivarGrafica()
 {
     if (spanFiltro)
     {
-        spanFiltro.style.display = "";;
+        spanFiltro.style.display = "none";;
     }
     canva.style.display = "none";
     filterList.style.display = "none";
