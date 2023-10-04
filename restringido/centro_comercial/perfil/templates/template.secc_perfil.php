236ff6a9f7fe84096ec681450f2b7e2e5459ed0a
PabloH126 <pabo120603@outlook.com>
2023-10-04 15:39:04 -0600

    filtro imagen de perfil 6

--- restringido/centro_comercial/perfil/templates/template.secc_perfil.php
+++ restringido/centro_comercial/perfil/templates/template.secc_perfil.php
@@ -4,4 +4,5 @@
 <link rel="stylesheet" type="text/css" href="css/header_profile.css">
 <link rel="stylesheet" type="text/css" href="css/aside_profile.css">
 <link rel="stylesheet" type="text/css" href="css/perfil_graficas.css">
-<link rel="stylesheet" type="text/css" href="css/perfil_graficas.css">
+<link rel="stylesheet" type="text/css" href="https://ustoree.azurewebsites.net/restringido/centro_comercial/tiendas/css/confirmacion_eliminacion.css">
+<link rel="stylesheet" type="text/css" href="https://ustoree.azurewebsites.net/restringido/centro_comercial/tiendas/css/notificacion_errores.css">
