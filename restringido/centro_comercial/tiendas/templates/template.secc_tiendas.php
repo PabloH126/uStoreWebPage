5bbf453ab0b5cbf50e635f924740c14465f7f69e
PabloH126 <pabo120603@outlook.com>
2023-10-04 20:54:32 -0600

    correcion crear productos 1

--- restringido/centro_comercial/tiendas/templates/template.secc_tiendas.php
+++ restringido/centro_comercial/tiendas/templates/template.secc_tiendas.php
@@ -1,2 +1,1 @@
 <link rel="stylesheet" type="text/css" href="https://ustoree.azurewebsites.net/restringido/centro_comercial/tiendas/css/tiendas.css">
-<link rel="stylesheet" type="text/css" href="../css/lista_tiendas.css">
