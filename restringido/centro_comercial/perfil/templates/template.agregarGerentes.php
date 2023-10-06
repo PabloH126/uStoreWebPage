c363190dd84b86b9ae0afd4a466f82ed0915bc1e
PabloH126 <pabo120603@outlook.com>
2023-10-06 13:30:34 -0600

    agregar gerente 2.3

--- restringido/centro_comercial/perfil/templates/template.agregarGerentes.php
+++ restringido/centro_comercial/perfil/templates/template.agregarGerentes.php
@@ -1,3 +1,4 @@
 <link rel="stylesheet" type="text/css" href="css/header_profile.css">
 <link rel="stylesheet" type="text/css" href="../css/utilidades.css">
 <link rel="stylesheet" type="text/css" href="css/secc_profile.css">
+<link rel="stylesheet" type="text/css" href="../css/formularios_creacion.css">
