3b29e68b1b20859f618ac6fc9e8922d92a52d612
PabloH126 <pabo120603@outlook.com>
2023-10-06 17:20:53 -0600

    agregar gerente 4.0.11

--- restringido/centro_comercial/perfil/agregar_gerente.php
+++ restringido/centro_comercial/perfil/agregar_gerente.php
@@ -110,6 +110,118 @@ curl_close($ch);
                         <button type="button" class="bttn-next" data-item="1" data-to_item="2"><i class='bx bx-right-arrow-alt bttn-next' data-item="1" data-to_item="2"></i></button>
                     </div>
                 </div>
+
+                <!-- Nombre de tienda-->
+                <div class="item active" id="item-1">
+                    <p>1/6</p>
+                    <div class="name">
+                        <label for="nombreTienda"><strong>Nombre de la tienda</strong></label>
+                        <input type="text" id="nombreTienda" name="nombreTienda">
+                    </div>
+                    <div class="bttn" id="one">
+                        <button type="button" class="bttn-next" data-item="1" data-to_item="2"><i class='bx bx-right-arrow-alt bttn-next' data-item="1" data-to_item="2"></i></button>
+                    </div>
+                </div>
+
+                <!-- Nombre de tienda-->
+                <div class="item active" id="item-1">
+                    <p>1/6</p>
+                    <div class="name">
+                        <label for="nombreTienda"><strong>Nombre de la tienda</strong></label>
+                        <input type="text" id="nombreTienda" name="nombreTienda">
+                    </div>
+                    <div class="bttn" id="one">
+                        <button type="button" class="bttn-next" data-item="1" data-to_item="2"><i class='bx bx-right-arrow-alt bttn-next' data-item="1" data-to_item="2"></i></button>
+                    </div>
+                </div>
+
+
+                <!-- Nombre de tienda-->
+                <div class="item active" id="item-1">
+                    <p>1/6</p>
+                    <div class="name">
+                        <label for="nombreTienda"><strong>Nombre de la tienda</strong></label>
+                        <input type="text" id="nombreTienda" name="nombreTienda">
+                    </div>
+                    <div class="bttn" id="one">
+                        <button type="button" class="bttn-next" data-item="1" data-to_item="2"><i class='bx bx-right-arrow-alt bttn-next' data-item="1" data-to_item="2"></i></button>
+                    </div>
+                </div>
+
+
+                <!-- Nombre de tienda-->
+                <div class="item active" id="item-1">
+                    <p>1/6</p>
+                    <div class="name">
+                        <label for="nombreTienda"><strong>Nombre de la tienda</strong></label>
+                        <input type="text" id="nombreTienda" name="nombreTienda">
+                    </div>
+                    <div class="bttn" id="one">
+                        <button type="button" class="bttn-next" data-item="1" data-to_item="2"><i class='bx bx-right-arrow-alt bttn-next' data-item="1" data-to_item="2"></i></button>
+                    </div>
+                </div>
+
+
+                <!-- Nombre de tienda-->
+                <div class="item active" id="item-1">
+                    <p>1/6</p>
+                    <div class="name">
+                        <label for="nombreTienda"><strong>Nombre de la tienda</strong></label>
+                        <input type="text" id="nombreTienda" name="nombreTienda">
+                    </div>
+                    <div class="bttn" id="one">
+                        <button type="button" class="bttn-next" data-item="1" data-to_item="2"><i class='bx bx-right-arrow-alt bttn-next' data-item="1" data-to_item="2"></i></button>
+                    </div>
+                </div>
+
+                <!-- Nombre de tienda-->
+                <div class="item active" id="item-1">
+                    <p>1/6</p>
+                    <div class="name">
+                        <label for="nombreTienda"><strong>Nombre de la tienda</strong></label>
+                        <input type="text" id="nombreTienda" name="nombreTienda">
+                    </div>
+                    <div class="bttn" id="one">
+                        <button type="button" class="bttn-next" data-item="1" data-to_item="2"><i class='bx bx-right-arrow-alt bttn-next' data-item="1" data-to_item="2"></i></button>
+                    </div>
+                </div>
+
+                <!-- Nombre de tienda-->
+                <div class="item active" id="item-1">
+                    <p>1/6</p>
+                    <div class="name">
+                        <label for="nombreTienda"><strong>Nombre de la tienda</strong></label>
+                        <input type="text" id="nombreTienda" name="nombreTienda">
+                    </div>
+                    <div class="bttn" id="one">
+                        <button type="button" class="bttn-next" data-item="1" data-to_item="2"><i class='bx bx-right-arrow-alt bttn-next' data-item="1" data-to_item="2"></i></button>
+                    </div>
+                </div>
+
+                <!-- Nombre de tienda-->
+                <div class="item active" id="item-1">
+                    <p>1/6</p>
+                    <div class="name">
+                        <label for="nombreTienda"><strong>Nombre de la tienda</strong></label>
+                        <input type="text" id="nombreTienda" name="nombreTienda">
+                    </div>
+                    <div class="bttn" id="one">
+                        <button type="button" class="bttn-next" data-item="1" data-to_item="2"><i class='bx bx-right-arrow-alt bttn-next' data-item="1" data-to_item="2"></i></button>
+                    </div>
+                </div>
+
+
+                <!-- Nombre de tienda-->
+                <div class="item active" id="item-1">
+                    <p>1/6</p>
+                    <div class="name">
+                        <label for="nombreTienda"><strong>Nombre de la tienda</strong></label>
+                        <input type="text" id="nombreTienda" name="nombreTienda">
+                    </div>
+                    <div class="bttn" id="one">
+                        <button type="button" class="bttn-next" data-item="1" data-to_item="2"><i class='bx bx-right-arrow-alt bttn-next' data-item="1" data-to_item="2"></i></button>
+                    </div>
+                </div>
                
             </form>
         </div>
