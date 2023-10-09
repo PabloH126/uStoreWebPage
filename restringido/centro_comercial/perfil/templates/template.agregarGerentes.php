5889a83b7453165ac53e5d4454ac962d215b9f03
PabloH126 <pabo120603@outlook.com>
2023-10-08 21:53:25 -0600

    prueba validacion nombre v1.2.2

--- restringido/centro_comercial/perfil/templates/template.agregarGerentes.php
+++ restringido/centro_comercial/perfil/templates/template.agregarGerentes.php
@@ -3,3 +3,5 @@
 <link rel="stylesheet" type="text/css" href="css/secc_profile.css">
 <link rel="stylesheet" type="text/css" href="../css/formularios_creacion.css">
 <link rel="stylesheet" type="text/css" href="css/agregar_gerentes.css">
+<link rel="stylesheet" type="text/css" href="https://ustoree.azurewebsites.net/restringido/centro_comercial/tiendas/css/confirmacion_eliminacion.css">
+<link rel="stylesheet" type="text/css" href="https://ustoree.azurewebsites.net/restringido/centro_comercial/tiendas/css/notificacion_errores.css">
