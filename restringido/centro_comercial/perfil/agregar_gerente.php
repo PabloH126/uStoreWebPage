28c70974e156d97109ec67ade77805cf70f63d4b
PabloH126 <pabo120603@outlook.com>
2023-10-09 20:03:37 -0600

    prueba de envio correo v1.3.4.1

--- restringido/centro_comercial/perfil/agregar_gerente.php
+++ restringido/centro_comercial/perfil/agregar_gerente.php
@@ -162,8 +162,8 @@ curl_close($ch);
                         </div>
                         <div class="bttn" id="next">
                             <button id="finalBtn" type="button" class="bttn-next" data-item="6" data-to_item="7">
-                                Correo<!--<i class='bx bx-right-arrow-alt bttn-next' data-item="6" data-to_item="7"></i>-->
-                                
+                                <!--<i class='bx bx-right-arrow-alt bttn-next' data-item="6" data-to_item="7"></i>-->
+                                <i class='bx bx-envelope bttn-next'></i>
                             </button>
                         </div>
                     </div>
