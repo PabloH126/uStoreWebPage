397e91320295708f8eb5a9f7337c74e2753f3bc0
PabloH126 <pabo120603@outlook.com>
2023-10-09 20:11:10 -0600

    prueba de envio correo v1.3.4.3

--- restringido/centro_comercial/perfil/agregar_gerente.php
+++ restringido/centro_comercial/perfil/agregar_gerente.php
@@ -177,7 +177,8 @@ curl_close($ch);
                         <input type="text" id="codigoConfirm" name="codigoConfirm"/>
                     </div>
                     <div class="notas">
-                        <span>* Se envió un código de confirmación a la dirección de correo electrónico proporcionada. En caso de no haberlo recibido, revisar carpeta de SPAM en su buzón</span><br>
+                        <span>* Se envió un código de confirmación a la dirección de correo electrónico proporcionada. <br>
+                            En caso de no haberlo recibido, revisar carpeta de SPAM en su buzón</span><br>
                         <span>* Ingrese el código para confirmar el registro del gerente.</span>
                     </div>
                     <div class="bttns">
