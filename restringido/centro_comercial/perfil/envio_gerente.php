0fc1c2f944b6e20b0a155eb947f3347b03a7dd9a
PabloH126 <pabo120603@outlook.com>
2023-10-12 14:05:54 -0600

    prueba subir imagen de perfil junto con gerente

--- restringido/centro_comercial/perfil/envio_gerente.php
+++ restringido/centro_comercial/perfil/envio_gerente.php
@@ -14,6 +14,7 @@ if($_POST['codigoConfirm'] != $_SESSION['claveConfirm'])
 }
 
 unset($_SESSION['claveConfirm']);
+$imagenPerfil = $_FILES['logoTienda'];
 
 $data = [
     'password' => $_POST['passwordGerente'],
@@ -36,7 +37,13 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 $response = curl_exec($ch);
 
 if ($response === false) {
-    echo 'Error: ' . curl_error($ch);
+    $responseArray = [
+        'status' => 'error',
+        'message' => curl_error($ch)
+    ];
+    echo json_encode($responseArray);
+    curl_close($ch);
+    exit;
 } else {
     $httpStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 }
@@ -52,12 +59,85 @@ if ($httpStatusCode != 201)
     exit;
 }
 
-$responseArray = [
-    'status' => 'success',
-    'message' => 'Gerente creado con éxito'
-];
+if (verificarImagen($imagenPerfil))
+{
+    $dataGerente = json_decode($response, true);
+    $dataImagen = [
+        'image' => curl_file_create($imagenPerfil['tmp_name'], $imagenPerfil['type'], $imagenPerfil['name'])
+    ];
 
-curl_close($ch);
-echo json_encode($responseArray);
-exit;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://ustoreapi.azurewebsites.net/api/Gerentes/UpdateProfileImage?idGerente=' . $dataGerente['idGerente']);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $dataImagen);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Authorization: Bearer ' . $_COOKIE['SessionToken']
+    ));
+
+    $response = curl_exec($ch);
+
+    if ($response === false) {
+        $responseArray = [
+            'status' => 'error',
+            'message' => curl_error($ch)
+        ];
+        echo json_encode($responseArray);
+        curl_close($ch);
+        exit;
+    } else {
+        $httpStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    }
+    
+    if ($httpStatusCode != 200)
+    {
+        $responseArray = [
+            'status' => 'error',
+            'message' => $response
+        ];
+        echo json_encode($responseArray);
+        curl_close($ch);
+        exit;
+    }
+
+    curl_close($ch);
+
+    $responseArray = [
+        'status' => 'success',
+        'message' => 'Gerente e imagen de perfil creados con éxito'
+    ];
+
+    curl_close($ch);
+    echo json_encode($responseArray);
+    exit;
+}
+else
+{
+    $responseArray = [
+        'status' => 'success',
+        'message' => 'Gerente creado con éxito'
+    ];
+
+    curl_close($ch);
+    echo json_encode($responseArray);
+    exit;
+}
+
+function verificarImagen($imagen) {
+    //Validación de imagenes
+    $allowedImageTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+    $maxSize = 1 * 1024 * 1024; // 1 megabyte
+
+    if(isset($imagen) && $imagen['error'] == 0) {
+        if(in_array($imagen['type'], $allowedImageTypes) && $imagen['size'] <= $maxSize) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    else
+    {
+        return false;
+    }
+}
 ?>
