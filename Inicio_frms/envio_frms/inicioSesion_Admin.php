a9b64910d3115beda4e6586d37facc2d67000a1e
PabloH126 <pabo120603@outlook.com>
2023-10-15 14:39:06 -0600

    prueba login gerente con redireccion a perfil tienda v1.1.2

--- Inicio_frms/envio_frms/inicioSesion_Admin.php
+++ Inicio_frms/envio_frms/inicioSesion_Admin.php
@@ -107,7 +107,7 @@ if (isset($_POST['emailAL']) && isset($_POST['passAL'])) {
             $_SESSION['email'] = $data['email'];
             $_SESSION['id'] = $data['id'];
             $_SESSION['UserType'] = $data['type'];
-            echo $_SESSION['UserType'];
+            echo $data;
             // redirigir al usuario a la página de inicio
             exit;
             if($_SESSION['UserType'] == "Administrador")
