7a2a183de49efff1afc31db26824478d61d09e5e
PabloH126 <pabo120603@outlook.com>
2023-10-15 18:58:21 -0600

    link general menu_gerentes

--- restringido/centro_comercial/templates/template.styles.php
+++ restringido/centro_comercial/templates/template.styles.php
@@ -10,3 +10,6 @@
 <!--ADD-->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 
+
+<!-- Gerentes -->
+<link rel="stylesheet" type="text/css" href="https://ustoree.azurewebsites.net/restringido/centro_comercial/css_gerentes/menu_gerentes.css">
