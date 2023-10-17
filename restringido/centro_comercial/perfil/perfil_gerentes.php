75c55530bd38d61193240d61bd84449bc48bd17a
PabloH126 <pabo120603@outlook.com>
2023-10-16 20:27:14 -0600

    prueba imagen de gerente actualizar

--- restringido/centro_comercial/perfil/perfil_gerentes.php
+++ restringido/centro_comercial/perfil/perfil_gerentes.php
@@ -72,6 +72,9 @@ curl_close($ch);
 
 <head>
 	<meta charset="utf-8">
+	<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+	<meta http-equiv="Pragma" content="no-cache" />
+	<meta http-equiv="Expires" content="0" />
 	<title>Perfil</title>
 	<?php require("../templates/template.styles.php"); ?>
 	<link rel="stylesheet" type="text/css" href="css/lista_gerentes.css">
