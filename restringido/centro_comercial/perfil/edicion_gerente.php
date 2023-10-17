3a882ac9542ed9caba106b3e933c5e308f2cc37d
PabloH126 <pabo120603@outlook.com>
2023-10-16 19:05:42 -0600

    prueba edicion gerente 2.0.1

--- restringido/centro_comercial/perfil/edicion_gerente.php
+++ restringido/centro_comercial/perfil/edicion_gerente.php
@@ -174,50 +174,6 @@ $gerenteData = json_decode($responseGerenteData, true);
                         </div>
                     </div>
                 </div>
-
-
-
-
-                <!-- Email del gerente-->
-                <div class="item" id="item-3">
-                    <p>3/7</p>
-                    <div class="name">
-                        <label for="correoGerente"><strong>Correo electrónico del gerente</strong></label>
-                        <input type="email" id="correoGerente" name="correoGerente">
-                    </div>
-                    <div class="bttns">
-                        <div class="bttn back">
-                            <button type="button" class="bttn-back" data-item="3" data-to_item="2"><i class='bx bx-left-arrow-alt bttn-back' data-item="3" data-to_item="2"></i></button>
-                        </div>
-                        <div class="bttn" id="next">
-                            <button id="btnEmail" type="button" class="bttn-next" data-item="3" data-to_item="4"><i class='bx bx-right-arrow-alt bttn-next' data-item="3" data-to_item="4"></i></button>
-                        </div>
-                    </div>
-                </div>
-
-                <!--Codigo de confirmacion -->
-                <div class="item" id="item-7">
-                    <p>7/7</p>
-                    <div class="name">
-                        <label for="codigoConfirm"><strong>Código de confirmación</strong></label>
-                        <input type="text" id="codigoConfirm" name="codigoConfirm" maxlength="8"/>
-                    </div>
-                    <div class="notas">
-                        <span>* Se envió un código de confirmación a la dirección de correo electrónico proporcionada. <br>
-                            En caso de no haberlo recibido, revise la carpeta de SPAM en su buzón.</span><br>
-                        <span>* Ingrese el código para confirmar el registro del gerente.</span>
-                    </div>
-                    <div class="bttns">
-                        <div class="bttn" id="send">
-                            <button type="submit" id="submitBtn">Guardar</button>
-                        </div>
-                    </div>
-                    <div id="volverCorreoBtn">
-                            <label id="volverEnviarCorreo">No recibí el correo</label>
-                    </div>
-                </div>
-
-            </form>
         </div>
     </div>
     <script src="../js/slider_formularios.js"></script>
