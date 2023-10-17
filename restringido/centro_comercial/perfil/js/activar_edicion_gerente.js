9f2a2333ead8e00ab2fbb23da15c1781128f399d
PabloH126 <pabo120603@outlook.com>
2023-10-16 23:34:43 -0600

    arreglo edcion de gerentes 3

--- restringido/centro_comercial/perfil/js/activar_edicion_gerente.js
+++ restringido/centro_comercial/perfil/js/activar_edicion_gerente.js
@@ -1,4 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const botonEdicion = document.querySelector('.edicionGerente');
+    const gerentes = document.querySelectorAll('.gerentes');
 
+    botonEdicion.addEventListener('click', function () {
+        gerentes.forEach(function (gerente) {
+            let idGerente = gerente.querySelector('.idGerente').value;
+            let link = gerente.querySelector('.myLink');
+
+            if (gerente.classList.contains('edit')) {
+                gerente.classList.remove('edit');
+                deshabilitarLink(link);
+            } else {
+                gerente.classList.add('edit');
+                habilitarLink(link, idGerente);
+            }
+        });
+    });
+
+    function habilitarLink(link, idGerente) {
+        link.setAttribute('href', 'https://ustoree.azurewebsites.net/restringido/centro_comercial/perfil/edicion_gerente.php?id=' + idGerente);
+        link.style.pointerEvents = 'auto'; // Habilita el clic en el enlace
+    }
+
+    function deshabilitarLink(link) {
+        link.removeAttribute('href');
+        link.style.pointerEvents = 'none'; // Deshabilita el clic en el enlace
+    }
+});
+
+
+
+/*
 document.addEventListener('DOMContentLoaded', function () {
     const botonEdicion = document.querySelector('.edicionGerente');
     const items = document.querySelectorAll('.gerentes');
@@ -30,4 +62,4 @@ document.addEventListener('DOMContentLoaded', function () {
         link.style.pointerEvents = 'none'; // Deshabilita el clic en el enlace
         linkEnabled = false;
     }
-});
+});*/
