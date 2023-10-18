9bc0c2df7d4c690e1e23147c8f8f00622d51f139
PabloH126 <pabo120603@outlook.com>
2023-10-18 17:43:24 -0600

    prueba chat v1.1.5

--- restringido/centro_comercial/chats/js/chat.js
+++ restringido/centro_comercial/chats/js/chat.js
@@ -2,26 +2,24 @@ let divsContacto = document.querySelectorAll('.contacto');
 const connection = new signalR.HubConnectionBuilder()
     .withUrl('https://ustoreapi.azurewebsites.net/chatHub')
     .build();
-
-connection.start()
-    .then(() => {
-        console.log('Conexion con SignalR exitosa');
-
-    })
-    .catch(err => {
-        console.error('Error al conectarse con SignalR: ', err);
-    });
-
 if(divsContacto)
 {
     divsContacto.forEach(chat => {
         let idChat = chat.querySelector('.idchat');
-        connection.invoke("JoinGroupChat", idChat)
+        connection.start()
         .then(() => {
-            console.log('Conexion con JoinGroupChat exitosa con el id:', idChat);
+            console.log('Conexion con SignalR exitosa');
+            connection.invoke("JoinGroupChat", idChat)
+            .then(() => {
+                console.log('Conexion con JoinGroupChat exitosa con el id:', idChat);
+            })
+            .catch(err => {
+                console.error('Hubo un error al conectarse con JoinGroupChat: ', err);
+            });
         })
         .catch(err => {
-            console.error('Hubo un error al conectarse con JoinGroupChat: ', err);
+            console.error('Error al conectarse con SignalR: ', err);
         });
+
     })
 }
