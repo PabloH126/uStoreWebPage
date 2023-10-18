3da0708a6ded6dad3795b14fcf8a0fd5fcc1091b
PabloH126 <pabo120603@outlook.com>
2023-10-17 18:46:12 -0600

    prueba bckground

--- restringido/centro_comercial/js/menu_desplegable.js
+++ restringido/centro_comercial/js/menu_desplegable.js
@@ -4,11 +4,13 @@ let menuIcon1 = document.getElementById('menu-icon');
 let menuIcon2 = document.getElementById('menu-icon2');
 let subMenu1 = document.getElementById('sub-menu');
 let subMenu2 = document.getElementById('sub-menu2');
+let backgroundAnimated = document.getElementById('divIconBackground');
 
 document.addEventListener('DOMContentLoaded', function () {
     menuIcon1.addEventListener('click', function (e) {
         subMenu1.classList.toggle("active");
         menuIcon1.classList.toggle("active");
+        backgroundAnimated.style.display = "none";
     });
 
     menuIcon2.addEventListener('click', function (e) {
