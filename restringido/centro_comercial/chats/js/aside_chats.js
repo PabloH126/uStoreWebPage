f30bba7160f78ace35286088c66f38265e0d3223
PabloH126 <pabo120603@outlook.com>
2023-10-18 22:42:14 -0600

    prueba chat v4.1.3.3

--- restringido/centro_comercial/chats/js/aside_chats.js
+++ restringido/centro_comercial/chats/js/aside_chats.js
@@ -17,7 +17,8 @@ contactos.forEach(contacto => {
 // Espera a que el DOM se cargue completamente
 document.addEventListener('DOMContentLoaded', function() {
     
-    var content = document.getElementById('contentTextarea');
+   /* var content = document.getElementById('contentTextarea');
+    var textarea = document.getElementById('expanding_textarea');*/
 /*/
     textarea.addEventListener('input', function() {
       // Resetea el campo de altura en caso de que se reduzca
@@ -32,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
     */
-    var textarea = document.getElementById('expanding_textarea');
+    const textarea = document.getElementById('expanding_textarea');
     const content = textarea.value;
     const lines = content.split('\n');
     let count = 0;
