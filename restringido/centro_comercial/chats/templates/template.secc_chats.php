46568a1f30b41f6be7cd6f97f5f66c1ac179d9a5
PabloH126 <pabo120603@outlook.com>
2023-10-20 22:14:51 -0600

    prueba chat v6.1.2

--- restringido/centro_comercial/chats/templates/template.secc_chats.php
+++ restringido/centro_comercial/chats/templates/template.secc_chats.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" type="text/css" href="../css/utilidades.css">
 <link rel="stylesheet" type="text/css" href="css/secc_chats.css">
 <link rel="stylesheet" type="text/css" href="../css/aside_general.css">
 <link rel="stylesheet" type="text/css" href="css/aside_chats.css">
