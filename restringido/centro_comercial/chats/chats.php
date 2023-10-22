8dba38392a7c59ea382676333f032154bdce747b
PabloH126 <pabo120603@outlook.com>
2023-10-22 15:25:12 -0600

    prueba chat v7.5.15

--- restringido/centro_comercial/chats/chats.php
+++ restringido/centro_comercial/chats/chats.php
@@ -3,7 +3,7 @@ session_start();
 require '../../security.php';
 	$ch = curl_init();
 
-	curl_setopt($ch, CURLOPT_URL, "https://ustoreapi.azurewebsites.net/api/Chat/GetChats?typeChat=Gerentes");
+	curl_setopt($ch, CURLOPT_URL, "https://ustoreapi.azurewebsites.net/api/Chat/GetChats?typeChat=Gerente");
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
