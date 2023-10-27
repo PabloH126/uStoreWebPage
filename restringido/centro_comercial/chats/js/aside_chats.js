f5d15021edb3c77671faecf3b9f4d603315756e8
PabloH126 <pabo120603@outlook.com>
2023-10-27 16:46:45 -0600

    chats v8.1

--- restringido/centro_comercial/chats/js/aside_chats.js
+++ restringido/centro_comercial/chats/js/aside_chats.js
@@ -109,9 +109,14 @@ function scrollToBottom() {
 searchBox.addEventListener('keyup', async function () {
     let busqueda = searchBox.value.toLowerCase();
     let typeChat = document.querySelector('.options_aside.selected').textContent;
-    await fetchChats(typeChat);
+    let contactos = document.querySelectorAll('.contacto_content');
+
     let contactosNombres = document.querySelectorAll('.contact_name');
-    let contadosFiltered = contactosNombres.filter(function (contacto) {
+
+
+
+    let contactosFiltered = contactosNombres.filter(function (contacto) {
+        console.log(contacto.textContent.toLowerCase().includes(busqueda));
         return contacto.textContent.toLowerCase().includes(busqueda);
     }).sort(function (a, b) {
         return a.textContent.localeCompare(b.textContent);
