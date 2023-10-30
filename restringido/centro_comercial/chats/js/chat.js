46766cc3df36760bd562da02c4a0483d1a0f6d0a
PabloH126 <pabo120603@outlook.com>
2023-10-29 20:37:41 -0600

    chats v8.6.6.3

--- restringido/centro_comercial/chats/js/chat.js
+++ restringido/centro_comercial/chats/js/chat.js
@@ -496,8 +496,8 @@ async function cambiarChatCreated(activar) {
                 let contactoSelectContainer = contactoSelect.closest('.contacto_content');
                 console.log(contactoSelectContainer);
                 if(contactoSelectContainer && (
-                    (contactoSelect.dataset.gerenteId == chat.idMiembro1 && chat.typeMiembro1 === "Gerente") || 
-                    (contactoSelect.dataset.gerenteId == chat.idMiembro2 && chat.typeMiembro2 === "Gerente" )))
+                    (contactoSelect.dataset.chatId == chat.idMiembro1 && chat.typeMiembro1 === "Gerente") || 
+                    (contactoSelect.dataset.chatId == chat.idMiembro2 && chat.typeMiembro2 === "Gerente" )))
                 {
                     console.log('entro al primer if');
                     crearMensaje(mensaje, idUser, gerenteId, chat.idChat);
