a0a71f3ac488e141b2824cfd7d271c46679066b7
NancyBan <nancyzendrero@gmail.com>
2023-12-29 16:09:03 -0600

    borrado de cositas

--- index.php
+++ index.php
@@ -55,11 +55,9 @@
 
 
 <!DOCTYPE html>
-<html lang="es">
+<html>
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<title>Inicio de sesión y registro</title>
 	
