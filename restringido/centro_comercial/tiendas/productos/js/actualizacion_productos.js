840ffb0fd7f06bd0eb2392ab1ae6d17db75807eb
PabloH126 <pabo120603@outlook.com>
2024-01-14 12:11:03 -0600

    prueba eliminacion de imagenes en submit  edicion tiendas y productos v1.4

--- restringido/centro_comercial/tiendas/productos/js/actualizacion_productos.js
+++ restringido/centro_comercial/tiendas/productos/js/actualizacion_productos.js
@@ -1,20 +1,3 @@
-let currentNotification;
-const imagenInput = document.getElementById('logoTienda');
-
-const imagenInput1 = document.getElementById('fileInput1');
-const imagenInput2 = document.getElementById('fileInput2');
-const imagenInput3 = document.getElementById('fileInput3');
-const imagenInput4 = document.getElementById('fileInput4');
-const imagenInput5 = document.getElementById('fileInput5');
-
-const imagenMostrada = document.getElementById('imagenSelec');
-
-const imagenMostrada1 = document.getElementById('imagenSelec1');
-const imagenMostrada2 = document.getElementById('imagenSelec2');
-const imagenMostrada3 = document.getElementById('imagenSelec3');
-const imagenMostrada4 = document.getElementById('imagenSelec4');
-const imagenMostrada5 = document.getElementById('imagenSelec5');
-
 document.addEventListener('DOMContentLoaded', function () {
     var checkboxes = document.querySelectorAll('.optionsC input[type="checkbox"]');
     var maxSelect = 8;
